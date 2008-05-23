5cd7a0489cd40d7172bfca9ad68de31323b40f47
Guillaume Bort <guillaume.bort@gmail.com>
2008-05-24 00:28:44 +0200

    start binding stuff

---
+++ framework/src/play/data/parsing/DataParser.java
@@ -0,0 +1,41 @@
+package play.data.parsing;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class DataParser {
+
+    public abstract Map<String, String[]> parse(InputStream is);
+    
+    // ~~~~~~~~ Repository 
+    
+    public static Map<String, DataParser> parsers = new HashMap<String, DataParser>();
+    
+    static {
+        parsers.put("application/x-www-form-urlencoded", new UrlEncodedParser());
+    }
+    
+    // ~~~~~~~~ Utils
+    
+    public static void putMapEntry(Map<String, String[]> map, String name, String value) {
+        String[] newValues = null;
+        String[] oldValues = map.get(name);
+        if (oldValues == null) {
+            newValues = new String[1];
+            newValues[0] = value;
+        } else {
+            newValues = new String[oldValues.length + 1];
+            System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
+            newValues[oldValues.length] = value;
+        }
+        map.put(name, newValues);
+    }
+
+    public static void putMapEntry(Map<String, String[]> map, String name, String[] values) {
+        for (String value : values) {
+            putMapEntry(map, name, value);
+        }
+    }
+    
+}
---
+++ framework/src/play/data/parsing/UrlEncodedParser.java
@@ -0,0 +1,78 @@
+package play.data.parsing;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UrlEncodedParser extends DataParser {
+
+    @Override
+    public Map<String, String[]> parse(InputStream is) {
+        try {
+            Map<String, String[]> params = new HashMap<String, String[]>();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            byte b;
+            while ((b = (byte) is.read()) > -1) {
+                os.write(b);
+            }
+            byte[] data = os.toByteArray();
+            int ix = 0;
+            int ox = 0;
+            String key = null;
+            String value = null;
+            while (ix < data.length) {
+                byte c = data[ix++];
+                switch ((char) c) {
+                    case '&':
+                        value = new String(data, 0, ox, "utf-8");
+                        if (key != null) {
+                            putMapEntry(params, key, value);
+                            key = null;
+                        }
+                        ox = 0;
+                        break;
+                    case '=':
+                        if (key == null) {
+                            key = new String(data, 0, ox, "utf-8");
+                            ox = 0;
+                        } else {
+                            data[ox++] = c;
+                        }
+                        break;
+                    case '+':
+                        data[ox++] = (byte) ' ';
+                        break;
+                    case '%':
+                        data[ox++] = (byte) ((convertHexDigit(data[ix++]) << 4) + convertHexDigit(data[ix++]));
+                        break;
+                    default:
+                        data[ox++] = c;
+                }
+            }
+            //The last value does not end in '&'.  So save it now.
+            if (key != null) {
+                value = new String(data, 0, ox, "utf-8");
+                putMapEntry(params, key, value);
+            }
+
+            putMapEntry(params, "body", new String(data, "utf-8"));
+            return params;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte convertHexDigit(byte b) {
+        if ((b >= '0') && (b <= '9')) {
+            return (byte) (b - '0');
+        }
+        if ((b >= 'a') && (b <= 'f')) {
+            return (byte) (b - 'a' + 10);
+        }
+        if ((b >= 'A') && (b <= 'F')) {
+            return (byte) (b - 'A' + 10);
+        }
+        return 0;
+    }
+}
