38d522578e683ca0aefde9f4ccf2022e812a3d68
Leo West <lwe@lwe>
2008-09-01 10:16:19 +0200

    mime list update

--- framework/src/play/mvc/results/NotFound.java
+++ framework/src/play/mvc/results/NotFound.java
@@ -5,10 +5,19 @@ import play.mvc.Http.Response;
 
 public class NotFound extends Result {
     
+    /**
+     * 
+     * @param why a description of the problem
+     */
     public NotFound(String why) {
         super(why);
     }
-    
+   
+    /**
+     * 
+     * @param method routed method
+     * @param path  routed path 
+     */
     public NotFound(String method, String path) {
         super(method + " " + path);
     }
