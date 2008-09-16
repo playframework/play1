8e85d4b84f2f43506b2dc221a433b85f8ad00ec5
Leo West <lwe@lwe>
2008-09-16 23:26:02 +0200

    add Content-Disposition:inline support in RenderBinary - improved filename support for attachment

--- framework/src/play/mvc/results/RenderBinary.java
+++ framework/src/play/mvc/results/RenderBinary.java
@@ -2,34 +2,83 @@ package play.mvc.results;
 
 import java.io.File;
 import java.io.InputStream;
+import org.apache.commons.codec.net.URLCodec;
 import play.exceptions.UnexpectedException;
 import play.mvc.Http.Request;
 import play.mvc.Http.Response;
 
 public class RenderBinary extends Result {
-    
+
+    private static URLCodec encoder = new URLCodec();
+    boolean inline = false;
     File file;
     InputStream is;
     String name;
     
+    /**
+     * send a binary stream as the response
+     * @param is the stream to read from
+     * @param name the name to use as Content-Diposition attachement filename
+     */
     public RenderBinary(InputStream is, String name) {
+        this( is, name, false );
+    }
+
+    /**
+     * send a binary stream as the response
+     * @param is the stream to read from
+     * @param name the name to use as Content-Diposition attachement filename
+     * @param inline true to set the response Content-Disposition to inline
+     */
+    public RenderBinary(InputStream is, String name, boolean inline ) {
         this.is = is;
         this.name = name;
+        this.inline = inline;
     }
     
+    /**
+     * Send a file as the response. Content-disposion is set to attachment.
+     * 
+     * @param file readable file to send back
+     * @param name a name to use as Content-disposion's filename
+     */
     public RenderBinary(File file, String name) {
+        this( file, name, false );
+    }
+
+    /**
+     * Send a file as the response. 
+     * Content-disposion is set to attachment, name is taken from file's name
+     * @param file readable file to send back
+     */
+    public RenderBinary(File file) {
+        this( file, file.getName(), false );
+    }
+
+    /**
+     * Send a file as the response. 
+     * Content-disposion is set to attachment, name is taken from file's name
+     * @param file readable file to send back
+     */
+    public RenderBinary(File file, String name, boolean inline) {
         this.file = file;
         this.name = name;
+        this.inline = inline;
     }
 
     @Override
     public void apply(Request request, Response response) {
         try {
             setContentTypeIfNotSet(response, "application/octet-stream");
-            if(name == null) {
-                response.setHeader("Content-disposition", "attachment");
+            if( inline ) {
+                response.setHeader("Content-Disposition", "inline");
+            } else if(name == null) {
+                response.setHeader("Content-Disposition", "attachment");
             } else {
-                response.setHeader("Content-disposition", "attachment; filename="+name);
+                // filename must be quoted and encoded (space, etc)
+                name = encoder.encode(name);
+                // see RFC2231 we dont support locale for now
+                response.setHeader("Content-Disposition", "attachment; filename*=utf-8'en-us'"+name);
             }
             if(file != null) {
             	if (!file.exists())
