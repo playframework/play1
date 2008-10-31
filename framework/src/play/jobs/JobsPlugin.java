61ee523363fb4a339969450513980401388323cf
Jean-Francois POUX <jfp@taldius.net>
2008-10-31 17:59:42 +0100

    Pass org.quartz.* properties from application.conf to quartz, default 1 thread

--- framework/src/play/jobs/JobsPlugin.java
+++ framework/src/play/jobs/JobsPlugin.java
@@ -2,12 +2,16 @@ package play.jobs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import play.PlayPlugin;
+import org.quartz.impl.StdSchedulerFactory;
+
 import play.Play;
+import play.PlayPlugin;
 import play.exceptions.JavaExecutionException;
 import play.exceptions.PlayException;
 import play.exceptions.UnexpectedException;
@@ -27,7 +31,18 @@ public class JobsPlugin extends PlayPlugin {
         }
         if (scheduler == null) {
             try {
-                scheduler = new org.quartz.impl.StdSchedulerFactory().getScheduler();
+            	StdSchedulerFactory factory = new StdSchedulerFactory();
+            	if (!Play.configuration.contains("org.quartz.threadPool.threadCount"))
+            		Play.configuration.put("org.quartz.threadPool.threadCount", "1");
+            	Properties props = new Properties ();
+            	props.load(this.getClass().getResourceAsStream("/org/quartz/quartz.properties"));
+            	for (Object key : Play.configuration.keySet()) {
+					if (key.toString().startsWith("org.quartz"))
+						props.put(key,Play.configuration.get(key));
+				}
+            	props.put("org.quartz.threadPool.threadCount",Play.configuration.getProperty("org.quartz.threadPool.threadCount", "2"));
+            	factory.initialize(props);
+                scheduler = factory.getScheduler();
                 scheduler.start();
             } catch (Exception e) {
                 throw new UnexpectedException("Cannot start scheduler");
@@ -73,7 +88,7 @@ public class JobsPlugin extends PlayPlugin {
                 scheduler.unscheduleJob(clazz.getName(), "play");
             }
         } catch (SchedulerException ex) {
-            throw new UnexpectedException("Cannot shitdow scheduler", ex);
+            throw new UnexpectedException("Cannot shutdow scheduler", ex);
         }
     }
 }
