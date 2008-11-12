f7dc8105cd882cff9f4c00d631a3fe0822bec210
Jean-Francois POUX <jfp@taldius.net>
2008-11-12 14:29:51 +0100

    Fix binder threading issue

--- framework/src/play/data/binding/CalendarBinder.java
+++ framework/src/play/data/binding/CalendarBinder.java
@@ -1,51 +1,38 @@
 package play.data.binding;
 
-import java.text.DateFormat;
-import java.text.FieldPosition;
 import java.text.ParseException;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 import java.util.Locale;
+
 import play.i18n.Lang;
 
-/**
- * a Date binder
- * 
- */
 public class CalendarBinder implements SupportedType<Calendar> {
 
-    // as SimpleDateFormat are not thread safe, we wrap it in a ThreadLocal
-    static ThreadLocal<AlternativeDateFormat> dateformat = new ThreadLocal<AlternativeDateFormat>();
+	static ThreadLocal<AlternativeDateFormat> dateformat = new ThreadLocal<AlternativeDateFormat>();
+
+    public static AlternativeDateFormat getFormatter () {
+    	if (dateformat.get()==null) {
+    		 dateformat.set(new AlternativeDateFormat(Locale.US,
+    	                "yyyy-MM-dd'T'hh:mm:ss'Z'", // ISO8601 + timezone
+    	                "yyyy-MM-dd'T'hh:mm:ss", // ISO8601
+    	                "yyyy-MM-dd",
+    	                "yyyyMMdd'T'hhmmss",
+    	                "yyyyMMddhhmmss",
+    	                "dd'/'MM'/'yyyy",
+    	                "dd-MM-yyyy",
+    	                "ddMMyyyy"));
+    	}
+    	return dateformat.get();
+    }
     
-
-    static {
-        dateformat.set(new AlternativeDateFormat(Locale.US,
-                "yyyy-MM-dd'T'hh:mm:ss'Z'", // ISO8601 + timezone
-                "yyyy-MM-dd'T'hh:mm:ss", // ISO8601
-                "yyyy-MM-dd",
-                "yyyyMMdd'T'hhmmss",
-                "yyyyMMddhhmmss",
-                "dd/MM/yyyy",
-                "dd-MM-yyyy",
-                "ddMMyyyy"));
-    }
-
-    public CalendarBinder() {
-    }
-
     public Calendar bind(String value) {
         try {
             Calendar cal;
-            if (Lang.get() != null && !"".equals(Lang.get())) {
+            if (Lang.get() != null && !"".equals(Lang.get()))
                 cal = Calendar.getInstance(new Locale(Lang.get()));
-            } else {
+            else
                 cal = Calendar.getInstance(Locale.getDefault());
-            }
-            cal.setTime(dateformat.get().parse(value));
+            cal.setTime(getFormatter().parse(value));
             return cal;
         } catch (ParseException ex) {
             play.Logger.warn("failed to parse calendar (%s)", value);
--- framework/src/play/data/binding/DateBinder.java
+++ framework/src/play/data/binding/DateBinder.java
@@ -4,29 +4,27 @@ import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * a Date binder
- * 
- */
 public class DateBinder implements SupportedType<Date> {
+	static ThreadLocal<AlternativeDateFormat> dateformat = new ThreadLocal<AlternativeDateFormat>();
 
-    // as SimpleDateFormat are not thread safe, we wrap it in a ThreadLocal
-    static ThreadLocal<AlternativeDateFormat> dateformat = new ThreadLocal<AlternativeDateFormat>();
-    static {
-        dateformat.set(new AlternativeDateFormat(Locale.US,
-                "yyyy-MM-dd'T'hh:mm:ss'Z'", // ISO8601 + timezone
-                "yyyy-MM-dd'T'hh:mm:ss", // ISO8601
-                "yyyy-MM-dd",
-                "yyyyMMdd'T'hhmmss",
-                "yyyyMMddhhmmss",
-                "dd/MM/yyyy",
-                "dd-MM-yyyy",
-                "ddMMyyyy"));
+    public static AlternativeDateFormat getFormatter () {
+    	if (dateformat.get()==null) {
+    		 dateformat.set(new AlternativeDateFormat(Locale.US,
+    	                "yyyy-MM-dd'T'hh:mm:ss'Z'", // ISO8601 + timezone
+    	                "yyyy-MM-dd'T'hh:mm:ss", // ISO8601
+    	                "yyyy-MM-dd",
+    	                "yyyyMMdd'T'hhmmss",
+    	                "yyyyMMddhhmmss",
+    	                "dd'/'MM'/'yyyy",
+    	                "dd-MM-yyyy",
+    	                "ddMMyyyy"));
+    	}
+    	return dateformat.get();
     }
 
     public Date bind(String value) {
         try {
-            return dateformat.get().parse(value);
+            return getFormatter().parse(value);
         } catch (ParseException ex) {
             play.Logger.warn("failed to parse date (%s)", value);
         }
