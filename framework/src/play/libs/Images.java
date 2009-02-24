f892c55c2c958d3f1b1d25a6535825d8029c1247
Loïc Guillois <loic.guillois@devcat.org>
2009-02-24 18:41:36 +0100

    adding crop function

--- framework/src/play/libs/Images.java
+++ framework/src/play/libs/Images.java
@@ -70,6 +70,48 @@ public class Images {
     }
 
     /**
+     * Crop an image
+     * @param originalImage The image file
+     * @param to The destination file
+     * @param x1 The new x origin
+     * @param y1 The new y origin
+     * @param x2 The new x end
+     * @param y2 The new y end
+     */
+    public static void crop(File originalImage, File to, Integer x1, Integer y1, Integer x2, Integer y2) {
+        try {
+            BufferedImage source = ImageIO.read(originalImage);
+
+            String mimeType = "image/jpeg";
+            if (to.getName().endsWith(".png")) {
+                mimeType = "image/png";
+            }
+            if (to.getName().endsWith(".gif")) {
+                mimeType = "image/gif";
+            }
+            int width = x2 - x1;
+            int height = y2 - y1;
+
+            // out
+            BufferedImage dest = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Image croppedImage = source.getSubimage(x1, y1, width, height);
+            Graphics graphics = dest.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, width, height);
+            graphics.drawImage(croppedImage, 0, 0, null);
+            ImageWriter writer = ImageIO.getImageWritersByMIMEType(mimeType).next();
+            ImageWriteParam params = writer.getDefaultWriteParam();
+            writer.setOutput(new FileImageOutputStream(to));
+            IIOImage image = new IIOImage(dest, null, null);
+            writer.write(null, image, params);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
      * Encode an image to base64
      * @param image The image file
      * @return The base64 encoded value
