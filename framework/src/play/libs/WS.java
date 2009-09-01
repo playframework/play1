9f2f80959d6d2e28281a262f3b34063ff595a2ae
Sébastien Crème <sebastiencreme@macbook-air-de-sebastien-creme-2.local>
2009-09-01 11:34:44 +0200

    add deprecated WS.GET ...

--- framework/src/play/libs/WS.java
+++ framework/src/play/libs/WS.java
@@ -213,7 +213,393 @@ public class WS extends PlayPlugin {
         states.get().setCredentials(authScope, credentials);
     }
 
-  
+    /**
+     * Make a GET request using an url template.
+     * If you provide no parameters, the url is left unchanged (no replacements)
+     * @param url an URL template with placeholders for parameters
+     * @param params a variable list of parameters to be replaced in the template using the String.format(...) syntax
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url, params...).get() instead.
+     */
+    public static HttpResponse GET(String url, Object... params) {
+        return GET(null, url, params);
+    }
+
+    /**
+     * Make a GET request using an url template.
+     * If you provide no parameters, the url is left unchanged (no replacements)
+     * @param url an URL template with placeholders for parameters
+     * @param params a variable Map
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).params(params).get() instead.
+     */
+    public static HttpResponse GET(String url, Map<String, String> params) {
+        StringBuffer sb = new StringBuffer(10);
+        for (String key : params.keySet()) {
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(key + "=" + encode(params.get(key)));
+        }
+        return GET(sb.toString());
+    }
+
+    /**
+     * Make a GET request using an url template.
+     * If you provide no parameters, the url is left unchanged (no replacements)
+     * @param headers a map of headers to be appended to the request.
+     * @param url an template with <tt>%s,%d</tt> placeholders for parameters.
+     * @param params parameters to be replaced in the url template using the String.format(...) syntax
+     * @return HTTP response {@link HttpResponse}
+     * @deprecated Use WS.url(url).params().header().get() instead.
+     */
+    public static HttpResponse GET(Map<String, Object> headers, String url, Object... params) {
+        if (params.length > 0) {
+            url = String.format(url, params);
+        }
+        if (getMethod.get() != null) {
+            getMethod.get().releaseConnection();
+        }
+        getMethod.set(new GetMethod(url));
+        getMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    getMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+            httpClient.executeMethod(null, getMethod.get(), states.get());
+            return new HttpResponse(getMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a POST request.
+     * @param url resource URL
+     * @param body content to be posted. content-type is guessed from the filename extension.
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).file().post() instead.
+     */
+    public static HttpResponse POST(String url, File body) {
+        String mimeType = MimeTypes.getMimeType(body.getName());
+        return POST(null, url, body, mimeType);
+    }
+
+    /**
+     * Make a POST request
+     * @param url resource URL
+     * @param body content to be posted
+     * @param mimeType the content type as a mimetype
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).file().mimetype().post() instead.
+     */
+    public static HttpResponse POST(String url, File body, String mimeType) {
+        return POST(null, url, body, mimeType);
+    }
+
+    /**
+     * Make a POST request
+     * @param headers a map of headers to be appended to the request.
+     * @param url resource URL
+     * @param body content to be posted. content-type is guessed from the filename extension.
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).header().file().post() instead.
+     */
+    public static HttpResponse POST(Map<String, String> headers, String url, File body) {
+        return POST(headers, url, body, null);
+    }
+
+    private static HttpResponse POST(Map<String, String> headers, String url, File body, String mimeType) {
+        if (postMethod.get() != null) {
+            postMethod.get().releaseConnection();
+        }
+        postMethod.set(new PostMethod(url));
+        postMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    postMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+            if (mimeType != null) {
+                postMethod.get().addRequestHeader("content-type", mimeType);
+            }
+
+            Part[] parts = {
+                new FilePart(body.getName(), body)
+            };
+
+            postMethod.get().setRequestEntity(new MultipartRequestEntity(parts, postMethod.get().getParams()));
+
+            httpClient.executeMethod(null, postMethod.get(), states.get());
+            return new HttpResponse(postMethod.get());
+        } catch (Exception e) {
+            postMethod.get().releaseConnection();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a POST request<p/>
+     * Parameters are encoded using a <tt>application/x-www-form-urlencoded</tt> scheme.
+     * You cannot send <tt>multipart/form-data</tt> for now.
+     * @param url the request URL
+     * @param parameters the parameters to be posted
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).params().post() instead.
+     */
+    public static HttpResponse POST(String url, Map<String, Object> parameters) {
+        return POST(null, url, parameters);
+    }
+
+    /**
+     * Make a POST request<p/>
+     * Parameters are encoded using a <tt>application/x-www-form-urlencoded</tt> scheme.
+     * You cannot send <tt>multipart/form-data</tt> for now.
+     * @param headers the request HTTP headers
+     * @param url the request URL
+     * @param parameters the parameters to be posted
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).params().header().post() instead.
+     */
+    public static HttpResponse POST(Map<String, Object> headers, String url, Map<String, Object> parameters) {
+        if (postMethod.get() != null) {
+            postMethod.get().releaseConnection();
+        }
+        postMethod.set(new PostMethod(url));
+        postMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    postMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+
+            postMethod.get().addRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+            ArrayList<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            Set<String> keySet = parameters.keySet();
+            for (String key : keySet) {
+                Object value = parameters.get(key);
+                if (value instanceof Collection) {
+                    for (Object v : (Collection) value) {
+                        NameValuePair nvp = new NameValuePair();
+                        nvp.setName(key);
+                        nvp.setValue(v.toString());
+                    }
+                } else {
+                    NameValuePair nvp = new NameValuePair();
+                    nvp.setName(key);
+                    nvp.setValue(value.toString());
+                }
+            }
+
+            postMethod.get().setRequestBody((NameValuePair[]) nvps.toArray());
+            httpClient.executeMethod(null, postMethod.get(), states.get());
+            return new HttpResponse(postMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * POST some text data
+     * @param url resource URL
+     * @param body text content to be posted "as is", with not encoding
+     * @param mimeType the request content-type
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url(url).params().body().mimetype().post() instead.
+     */
+    public static HttpResponse POST(String url, String body, String mimeType) {
+        return POST((Map<String, Object>) null, url, body);
+    }
+
+    /**
+     * POST some text data
+     * @param headers extra request headers
+     * @param url resource URL
+     * @param body text content to be posted "as is"
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().headers().body().post() instead
+     */
+    public static HttpResponse POST(Map<String, Object> headers, String url, String body) {
+        if (postMethod.get() != null) {
+            postMethod.get().releaseConnection();
+        }
+        postMethod.set(new PostMethod(url));
+        postMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    postMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+
+            postMethod.get().setRequestEntity(new StringRequestEntity(body));
+
+            httpClient.executeMethod(null, postMethod.get(), states.get());
+            return new HttpResponse(postMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a DELETE request
+     * @param url resource URL
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().delete() instead.
+     */
+    public static HttpResponse DELETE(String url) {
+        return DELETE(null, url);
+    }
+
+    /**
+     * Make a DELETE request
+     * @param headers extra request headers
+     * @param url resource URL
+     * @param params query parameters
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().headers().delete() instead.
+     */
+    public static HttpResponse DELETE(Map<String, String> headers, String url) {
+        if (deleteMethod.get() != null) {
+            deleteMethod.get().releaseConnection();
+        }
+        deleteMethod.set(new DeleteMethod(url));
+        deleteMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    deleteMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+
+            httpClient.executeMethod(null, deleteMethod.get(), states.get());
+            return new HttpResponse(deleteMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a HEAD request
+     * @param url resource URL
+     * @param params query parameters
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().head() instead.
+     */
+    public static HttpResponse HEAD(String url, Object... params) {
+        return HEAD(null, url, params);
+    }
+
+    /**
+     * Make a HEAD request
+     * @param headers extra request headers
+     * @param url resource URL
+     * @param params query parameters
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().headers().params().head() instead.
+     */
+    public static HttpResponse HEAD(Map<String, String> headers, String url, Object... params) {
+        url = String.format(url, params);
+        if (headMethod.get() != null) {
+            headMethod.get().releaseConnection();
+        }
+        headMethod.set(new HeadMethod(url));
+        headMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    headMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+            httpClient.executeMethod(null, headMethod.get(), states.get());
+            return new HttpResponse(headMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a TRACE request
+     * @param url resource URL
+     * @param params query parameters
+     * @return server response {@link HttpResponse}
+     * @deprecated Use WS.url().trace() instead.
+     */
+    public static HttpResponse TRACE(String url, Object... params) {
+        return TRACE(null, url, params);
+    }
+
+    /**
+     * Make a TRACE request
+     * @param headers extra request headers
+     * @param url resource URL
+     * @param params query parameters
+     * @return the response
+     * @deprecated Use WS.url().headers().params().delete() instead.
+     */
+    public static HttpResponse TRACE(Map<String, String> headers, String url, Object... params) {
+        url = String.format(url, params);
+        if (traceMethod.get() != null) {
+            traceMethod.get().releaseConnection();
+        }
+        traceMethod.set(new TraceMethod(url));
+        traceMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    traceMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+            httpClient.executeMethod(null, traceMethod.get(), states.get());
+            return new HttpResponse(traceMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Make a OPTIONS request
+     * @param url resource URL
+     * @param params query parameters
+     * @return the OPTIONS response
+     * @deprecated Use WS.url().options() instead.
+     */
+    public static HttpResponse OPTIONS(String url, Object... params) {
+        return OPTIONS(null, url, params);
+    }
+
+    /**
+     * Make a OPTIONS request
+     * @param headers extra request headers
+     * @param url resource URL
+     * @param params query parameters
+     * @return the OPTIONS response
+     * @deprecated Use WS.url().headers().params().options() instead.
+     */
+    public static HttpResponse OPTIONS(Map<String, String> headers, String url, Object... params) {
+        url = String.format(url, params);
+        if (optionsMethod.get() != null) {
+            optionsMethod.get().releaseConnection();
+        }
+        optionsMethod.set(new OptionsMethod(url));
+        optionsMethod.get().setDoAuthentication(true);
+        try {
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    optionsMethod.get().addRequestHeader(key, headers.get(key) + "");
+                }
+            }
+            httpClient.executeMethod(null, optionsMethod.get(), states.get());
+            return new HttpResponse(optionsMethod.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * URL-encode an UTF-8 string to be used as a query string parameter.
@@ -355,6 +741,7 @@ public class WS extends PlayPlugin {
         }
         
         /**
+         * Set the headers of the request.
          * Add a header to the request
          * @param name header name
          * @param value header value
