5103d5f5e2957a8d552ae2d8e76a11731ed12e3b
guillaume <guillaume@macbook-pro-de-guillaume.local>
2009-10-26 18:55:02 +0100

    Partially fix #460626

--- framework/src/javax/inject/Inject.java
+++ framework/src/javax/inject/Inject.java
@@ -5,7 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Inject {
+
 }
---
+++ framework/src/play/inject/Injector.java
@@ -0,0 +1,34 @@
+package play.inject;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.List;
+import javax.inject.Inject;
+import play.Play;
+import play.mvc.Controller;
+
+public class Injector {
+    
+    /**
+     * For now, inject beans in controllers
+     */
+    public static void inject(BeanSource source) {
+        List<Class> classes = Play.classloader.getAssignableClasses(Controller.class);
+        for(Class clazz : classes) {
+            for(Field field : clazz.getDeclaredFields()) {
+                if(Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(Inject.class)) {
+                    Class type = field.getType();
+                    field.setAccessible(true);
+                    try {
+                        field.set(null, source.getBeanOfType(type));
+                    } catch(RuntimeException e) {
+                        throw e;
+                    } catch(Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+
+}
