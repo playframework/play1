7738c243c1a2a669e253a80a5c66fab93ef0a5c4
guillaume <guillaume@gbo-mac>
2009-12-09 21:09:44 +0100

    Oops

--- modules/secure/app/controllers/Secure.java
+++ modules/secure/app/controllers/Secure.java
@@ -41,7 +41,7 @@ public class Secure extends Controller {
 
     public static void login() throws Throwable {
         Http.Cookie remember = request.cookies.get("rememberme");
-        if(remember != null && remember.indexOf("-") > 0) {
+        if(remember != null && remember.value.indexOf("-") > 0) {
             String sign = remember.value.substring(0, remember.value.indexOf("-"));
             String username = remember.value.substring(remember.value.indexOf("-") + 1);
             if(Crypto.sign(username).equals(sign)) {
