03b0c2054f21efeb11a4d7bffc3ef68170a48606
tomas <gugljafa@zoznam.sk>
2009-12-11 15:47:37 +0100

    basic support for type safe functional testing

--- samples-and-tests/just-test-cases/test/BinaryTest.java
+++ samples-and-tests/just-test-cases/test/BinaryTest.java
@@ -6,8 +6,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import play.mvc.Http.Response;
+import play.mvc.results.Redirect;
 import play.test.Fixtures;
 import play.test.FunctionalTest;
+import controllers.Binary;
 
 
 public class BinaryTest extends FunctionalTest {
@@ -17,17 +19,33 @@ public class BinaryTest extends FunctionalTest {
 	public void setUp() {
 		Fixtures.deleteAttachmentsDir();
 		// Fixtures.deleteAll(); // see Bug #491403
-		Response deletedResponse = GET("/binary/deleteAll");
+		String deleteURL=null;
+		try{
+			Binary.deleteAll();
+		}catch(Redirect redirect){
+			deleteURL=redirect.url;
+		}		
+		Response deletedResponse = GET(deleteURL);
 		assertStatus(200, deletedResponse);
 	}
 	
 	@Test
 	public void testUploadSomething(){
-		String imageURL = "/binary/showavatar?id=1";
+		String imageURL=null;
+		try{
+			Binary.showAvatar(1l);
+		}catch(Redirect redirect){
+			imageURL=redirect.url;
+		}
 		Response getResponse = GET(imageURL);
 		assertStatus(404, getResponse);
 		
-		String url="/binary/save";
+		String url=null;
+		try{
+			Binary.save(null);
+		}catch(Redirect redirect){
+			url = redirect.url;
+		}
 		Map<String,String> parameters= new HashMap<String,String>();
 		parameters.put("user.username", "username");
 		Map<String, File> files= new HashMap<String, File>();
@@ -37,7 +55,12 @@ public class BinaryTest extends FunctionalTest {
 		Response uploadResponse = POST(url, parameters, files);
 		assertStatus(302, uploadResponse);
 		String id = uploadResponse.getHeader("Location").split("=")[1];
-		imageURL = "/binary/showavatar?id=" + id;
+		imageURL = null;
+		try{
+			Binary.showAvatar(new Long(id));
+		}catch(Redirect redirect){
+			imageURL = redirect.url;
+		}
 		getResponse = GET(imageURL);
 		assertStatus(200, getResponse);
 	}
