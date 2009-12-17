0ceca32e890733a846ad6bddcdf4360e56f76bc4
Nicolas Leroux <nicolas@lunatech.com>
2009-12-17 16:27:24 +0100

    Added missing annotations and removed generated lib

---
+++ modules/router/src/play/modules/router/StaticRoutes.java
@@ -0,0 +1,15 @@
+package play.modules.router;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface StaticRoutes {
+
+   ServeStatic[] serveStatics();
+
+}
