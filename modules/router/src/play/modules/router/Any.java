2931e62e92494cdd5d1e5f17a5518f4f9f2bb396
Nicolas Leroux <nicolas@lunatech.com>
2009-12-18 12:19:37 +0100

    Forgot files for https://bugs.launchpad.net/play/+bug/497554

---
+++ modules/router/src/play/modules/router/Any.java
@@ -0,0 +1,20 @@
+package play.modules.router;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Any {
+
+    public abstract String value();
+
+    public abstract String params() default "";
+
+    public abstract int priority() default -1;
+
+}
