edd63abcc3b0379ede983884ddd06a4b60605940
Sylvain MATHIEU <sma@sma-laptop>
2010-02-03 14:16:18 +0100

    Allows quote escapes in selenium commands

--- framework/src/play/test/Helpers.java
+++ framework/src/play/test/Helpers.java
@@ -5,18 +5,19 @@ import java.util.regex.Pattern;
 
 public class Helpers {
 
-    static Pattern pattern = Pattern.compile("^(\\w+)\\(\\s*('[^']*'|[^,)]*)\\s*,?\\s*('[^']*'|[^,)]*)?\\s*\\)$");
+    static Pattern pattern = Pattern.compile("^(\\w+)\\(\\s*(?:('(?:\\\\'|[^'])*'|[^.]+?)\\s*(?:,\\s*('(?:\\\\'|[^'])*'|[^.]+?)\\s*)?)?\\)$");
 
     public static String[] seleniumCommand(String command) {
         Matcher matcher = pattern.matcher(command.trim());
         if (matcher.matches()) {
             String[] result = new String[3];
             result[0] = matcher.group(1);
-            result[1] = matcher.group(2);
-            result[2] = matcher.group(3);
+            result[1] = matcher.group(2)!=null?matcher.group(2):"";
+            result[2] = matcher.group(3)!=null?matcher.group(3):"";
             for (int i = 0; i < result.length; i++) {
                 if (result[i].matches("^'.*'$")) {
                     result[i] = result[i].substring(1, result[i].length() - 1);
+                    result[i] = result[i].replace("\\'", "'");
                 }
             }
             return result;
