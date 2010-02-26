0c6b8d3ed0eedbb8b26c56b6b321d9d5dd0b3685
Erwan Loisant <erwan@coot>
2010-02-26 21:24:52 +0100

    Revert a change that wasn't supposed to be committed

--- modules/test-runner/app/controllers/TestRunner.java
+++ modules/test-runner/app/controllers/TestRunner.java
@@ -1,13 +1,7 @@
 package controllers;
 
 import java.io.File;
-import java.lang.reflect.Type;
 import java.util.*;
-
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
 import play.Logger;
 import play.Play;
@@ -17,38 +11,19 @@ import play.mvc.*;
 import play.templates.Template;
 import play.templates.TemplateLoader;
 import play.test.*;
-import play.test.TestEngine.TestResult;
-import play.test.TestEngine.TestResults;
 import play.vfs.*;
 
 public class TestRunner extends Controller {
 
-    public static void index(String format) {
+    public static void index() {
         List<Class> unitTests = TestEngine.allUnitTests();
         List<Class> functionalTests = TestEngine.allFunctionalTests();
         List<String> seleniumTests = TestEngine.allSeleniumTests();
-        if (format != null && format.equals("json")) {
-        	Map<String, List<String>> result = new HashMap<String, List<String>>();
-        	List<String> unitTestsNames = new ArrayList<String>();
-        	for (int i = 0; i < unitTests.size(); i++) {
-        		unitTestsNames.add(unitTests.get(i).getName());
-        	}
-        	List<String> functionalTestsNames = new ArrayList<String>();
-        	for (int i = 0; i < functionalTests.size(); i++) {
-        		functionalTestsNames.add(functionalTests.get(i).getName());
-        	}
-        	result.put("unitTests", unitTestsNames);
-        	result.put("functionalTests", functionalTestsNames);
-        	result.put("seleniumTests", seleniumTests);
-        	renderJSON(result);
-        } else {
-        	render(unitTests, functionalTests, seleniumTests);
-        }
+        render(unitTests, functionalTests, seleniumTests);
     }
 
-    public static void run(String test, String format) throws Exception {
-    	if (format == null || format.isEmpty()) format = "html";
-    	if (test.equals("init")) {
+    public static void run(String test) throws Exception {
+        if (test.equals("init")) {
             File testResults = Play.getFile("test-result");
             if (!testResults.exists()) {
                 testResults.mkdir();
@@ -69,23 +44,16 @@ public class TestRunner extends Controller {
             Play.getFile("test-result").mkdir();
             java.lang.Thread.sleep(250);
             TestEngine.TestResults results = TestEngine.run(test.substring(0, test.length() - 6));
-            if (format.equals("json")) {
-                response.status = 200; // 500 prevents some clients to get the body
-                String json = new Gson().toJson(results);
-                response.contentType = "application/json";
-                renderText(json);
-            } else {
-                response.status = results.passed ? 200 : 500;
-                Map<String, Object> options = new HashMap<String, Object>();
-                options.put("test", test);
-                options.put("results", results);
-                Template resultTemplate = TemplateLoader.load("TestRunner/results.html");
-                String result = resultTemplate.render(options);
-                File testResults = Play.getFile("test-result/" + test.replace(".class", ".java") + (results.passed ? ".passed" : ".failed") + ".html");
-                IO.writeContent(result, testResults);
-                response.contentType = "text/html";
-                renderText(result);
-            }
+            response.status = results.passed ? 200 : 500;
+            Template resultTemplate = TemplateLoader.load("TestRunner/results.html");
+            Map<String, Object> options = new HashMap<String, Object>();
+            options.put("test", test);
+            options.put("results", results);
+            String result = resultTemplate.render(options);
+            File testResults = Play.getFile("test-result/" + test.replace(".class", ".java") + (results.passed ? ".passed" : ".failed") + ".html");
+            IO.writeContent(result, testResults);
+            response.contentType = "text/html";
+            renderText(result);
         }
         if (test.endsWith(".test.html.suite")) {
             test = test.substring(0, test.length() - 6);
