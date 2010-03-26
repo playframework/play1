208123c16e4d216287cf559dd88518739928599f
Nicolas Leroux <nicolas@lunatech.com>
2010-03-26 10:17:12 +0100

    Authentify is deprecated. From now on it is authenticate. However, we remain of course backward compatible.

--- modules/secure/app/controllers/Secure.java
+++ modules/secure/app/controllers/Secure.java
@@ -55,7 +55,15 @@ public class Secure extends Controller {
 
     public static void authenticate(@Required String username, String password, boolean remember) throws Throwable {
         // Check tokens
-        if(validation.hasErrors() || !(Boolean)Security.invoke("authentify", username, password)) {
+        Boolean allowed = false;
+        try {
+            // This is the official method name
+            allowed = (Boolean)Security.invoke("authenticate", username, password);
+        } catch (NoSuchMethodException e ) {
+              // This is the deprecated method name
+            allowed = (Boolean)Security.invoke("authentify", username, password);
+        }
+        if(validation.hasErrors() || !allowed) {
             flash.keep("url");
             flash.error("secure.error");
             params.flash();
@@ -92,28 +100,75 @@ public class Secure extends Controller {
 
     public static class Security extends Controller {
 
+        /**
+         * @Deprecated
+         * 
+         * @param username
+         * @param password
+         * @return
+         */
         static boolean authentify(String username, String password) {
             return true;
         }
 
+        /**
+         * This method is called during the authentication process. This is where you check if
+         * the user is allowed to log in into the system. This is the actual authentication process
+         * agaisnt a third party system (most of the time a DB).
+         *
+         * @param username
+         * @param password
+         * @return true if the authentication process succeeded
+         */
+        static boolean authenticate(String username, String password) {
+            return true;
+        }
+
+        /**
+         * This method checks that a profile is allowed to view this page/method. This method is called prior
+         * to the method's controller annotated with the @Check method. 
+         *
+         * @param profile
+         * @return true if you are allowed to execute this controller method.
+         */
         static boolean check(String profile) {
             return true;
         }
 
+        /**
+         * This method returns the current connected username
+         * @return
+         */
         static String connected() {
             return session.get("username");
         }
-        
+
+        /**
+         * Indicate if a user is currently connected
+         * @return  true if the user is connected
+         */
         static boolean isConnected() {
             return session.contains("username");
         }
-        
+
+        /**
+         * This method is called after a successful authentication.
+         * You need to override this method if you with to perform specific actions (eg. Record the time the user signed in)
+         */
         static void onAuthenticated() {
         }
-        
+
+         /**
+         * This method is called after a successful sign off.
+         * You need to override this method if you with to perform specific actions (eg. Record the time the user signed off)
+         */
         static void onDisconnected() {
         }
-        
+
+        /**
+         * This method is called if a check does not succeed. By default it shows the not allowed page (the controller forbidden method).
+         * @param profile
+         */
         static void onCheckFailed(String profile) {
             forbidden();
         }
