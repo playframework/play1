7a1b7f50ea0d08cc6fcdf21049455436e9c1d9a8
Erwan Loisant <erwan@coot>
2010-03-29 15:02:22 +0200

    Javadoc and Precompile

---
+++ framework/pym/commands/javadoc.py
@@ -0,0 +1,39 @@
+import os, os.path
+import shutil
+import subprocess
+
+from framework.pym.utils import *
+
+NAMES = ['javadoc', 'jd']
+
+def execute(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    play_env = kargs.get("env")
+
+    app.check()
+    modules = app.modules()
+    if not os.environ.has_key('JAVA_HOME'):
+        javadoc_path = "javadoc"
+    else:
+        javadoc_path = os.path.normpath("%s/bin/javadoc" % os.environ['JAVA_HOME'])
+
+    fileList = []
+    def add_java_files(app_path):
+        for root, subFolders, files in os.walk(os.path.join(app_path, 'app')):
+            for file in files:
+                if file.endswith(".java"):
+                    fileList.append(os.path.join(root, file))
+    add_java_files(app.path)
+    for module in modules:
+        add_java_files(os.path.normpath(module))
+    outdir = os.path.join(app.path, 'javadoc')
+    sout = open(os.path.join(app.log_path(), 'javadoc.log'), 'w')
+    serr = open(os.path.join(app.log_path(), 'javadoc.err'), 'w')
+    if (os.path.isdir(outdir)):
+        shutil.rmtree(outdir)
+    javadoc_cmd = [javadoc_path, '-classpath', app.cp_args(), '-d', outdir] + fileList
+    print "Generating Javadoc in " + outdir + "..."
+    subprocess.call(javadoc_cmd, env=os.environ, stdout=sout, stderr=serr)
+    print "Done! You can open " + os.path.join(outdir, 'overview-tree.html') + " in your browser."
---
+++ framework/pym/commands/precompile.py
@@ -0,0 +1,27 @@
+import os, os.path
+import shutil
+import subprocess
+
+from framework.pym.utils import *
+
+NAMES = ['precompile']
+
+def execute(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    play_env = kargs.get("env")
+
+    app.check()
+    java_cmd = app.java_cmd(args)
+    if os.path.exists(os.path.join(app.path, 'tmp')):
+        shutil.rmtree(os.path.join(app.path, 'tmp'))
+    if os.path.exists(os.path.join(app.path, 'precompiled')):
+        shutil.rmtree(os.path.join(app.path, 'precompiled'))
+    java_cmd.insert(2, '-Dprecompile=yes')
+    try:
+        subprocess.call(java_cmd, env=os.environ)
+    except OSError:
+        print "Could not execute the java executable, please make sure the JAVA_HOME environment variable is set properly (the java executable should reside at JAVA_HOME/bin/java). "
+        sys.exit(-1)
+    print
