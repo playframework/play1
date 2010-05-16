14ab71315401f445f58d7961eb488325da300009
Nicolas Leroux <nicolas@lunatech.com>
2010-05-16 21:49:03 +0200

    Guess more dialects

--- framework/src/play/db/jpa/JPAPlugin.java
+++ framework/src/play/db/jpa/JPAPlugin.java
@@ -36,20 +36,20 @@ public class JPAPlugin extends PlayPlugin {
 
     @Override
     public Object bind(String name, Class clazz, java.lang.reflect.Type type, Map<String, String[]> params) {
-        if(!Play.configuration.getProperty("future.bindJPAObjects", "false").equals("true")) {
+        if (!Play.configuration.getProperty("future.bindJPAObjects", "false").equals("true")) {
             return null;
         }
         // TODO need to be more generic in order to work with JPASupport
-        if(JPASupport.class.isAssignableFrom(clazz)) {
-            String idKey = name + ".id"; 
-            if(params.containsKey(idKey) && params.get(idKey).length > 0 && params.get(idKey)[0] != null && params.get(idKey)[0].trim().length() > 0) {
+        if (JPASupport.class.isAssignableFrom(clazz)) {
+            String idKey = name + ".id";
+            if (params.containsKey(idKey) && params.get(idKey).length > 0 && params.get(idKey)[0] != null && params.get(idKey)[0].trim().length() > 0) {
                 String id = params.get(idKey)[0];
                 try {
                     Query query = JPA.em().createQuery("from " + clazz.getName() + " o where o.id = ?");
                     query.setParameter(1, play.data.binding.Binder.directBind(id + "", play.db.jpa.JPASupport.findKeyType(clazz)));
                     Object o = query.getSingleResult();
                     return JPASupport.edit(o, name, params);
-                } catch(Exception e) {
+                } catch (Exception e) {
                     return null;
                 }
             }
@@ -76,7 +76,7 @@ public class JPAPlugin extends PlayPlugin {
             if (DB.datasource != null) {
                 cfg.setDataSource(DB.datasource);
             }
-            
+
             if (!Play.configuration.getProperty("jpa.ddl", "update").equals("none")) {
                 cfg.setProperty("hibernate.hbm2ddl.auto", Play.configuration.getProperty("jpa.ddl", "update"));
             }
@@ -164,11 +164,11 @@ public class JPAPlugin extends PlayPlugin {
                 }
             }
             String[] moreEntities = Play.configuration.getProperty("jpa.entities", "").split(", ");
-            for(String entity : moreEntities) {
-                if(entity.trim().equals("")) continue;
+            for (String entity : moreEntities) {
+                if (entity.trim().equals("")) continue;
                 try {
                     cfg.addAnnotatedClass(Play.classloader.loadClass(entity));
-                } catch(Exception e) {
+                } catch (Exception e) {
                     Logger.warn("JPA -> Entity not found: %s", entity);
                 }
             }
@@ -183,19 +183,51 @@ public class JPAPlugin extends PlayPlugin {
     }
 
     static String getDefaultDialect(String driver) {
-        if (driver.equals("org.hsqldb.jdbcDriver")) {
+        String dialect = Play.configuration.getProperty("jpa.dialect");
+        if (dialect != null) {
+            return dialect;
+        } else if (driver.equals("org.hsqldb.jdbcDriver")) {
             return "org.hibernate.dialect.HSQLDialect";
         } else if (driver.equals("com.mysql.jdbc.Driver")) {
             return "play.db.jpa.MySQLDialect";
+        } else if (driver.equals("org.postgresql.Driver")) {
+            return "org.hibernate.dialect.PostgreSQLDialect";
+        } else if (driver.toLowerCase().equals("com.ibm.db2.jdbc.app.DB2Driver")) {
+            return "org.hibernate.dialect.DB2Dialect";
+        } else if (driver.equals("com.ibm.as400.access.AS400JDBCDriver")) {
+            return "org.hibernate.dialect.DB2400Dialect";
+        } else if (driver.equals("com.ibm.as400.access.AS390JDBCDriver")) {
+            return "org.hibernate.dialect.DB2390Dialect";
+        } else if (driver.equals("oracle.jdbc.driver.OracleDriver")) {
+            return "org.hibernate.dialect.Oracle9iDialect";
+        } else if (driver.equals("com.sybase.jdbc2.jdbc.SybDriver")) {
+            return "org.hibernate.dialect.SybaseAnywhereDialect";
+        } else if ("com.microsoft.jdbc.sqlserver.SQLServerDriver".equals(driver)) {
+            return "org.hibernate.dialect.SQLServerDialect";
+        } else if ("com.sap.dbtech.jdbc.DriverSapDB".equals(driver)) {
+            return "org.hibernate.dialect.SAPDBDialect";
+        } else if ("com.informix.jdbc.IfxDriver".equals(driver)) {
+            return "org.hibernate.dialect.InformixDialect";
+        } else if ("com.ingres.jdbc.IngresDriver".equals(driver)) {
+            return "org.hibernate.dialect.IngresDialect";
+        } else if ("progress.sql.jdbc.JdbcProgressDriver".equals(driver)) {
+            return "org.hibernate.dialect.ProgressDialect";
+        } else if ("com.mckoi.JDBCDriver".equals(driver)) {
+            return "org.hibernate.dialect.MckoiDialect";
+        } else if ("InterBase.interclient.Driver".equals(driver)) {
+            return "org.hibernate.dialect.InterbaseDialect";
+        } else if ("com.pointbase.jdbc.jdbcUniversalDriver".equals(driver)) {
+            return "org.hibernate.dialect.PointbaseDialect";
+        } else if ("com.frontbase.jdbc.FBJDriver".equals(driver)) {
+            return "org.hibernate.dialect.FrontbaseDialect";
+        } else if ("org.firebirdsql.jdbc.FBDriver".equals(driver)) {
+            return "org.hibernate.dialect.FirebirdDialect";
         } else {
-            String dialect = Play.configuration.getProperty("jpa.dialect");
-            if (dialect != null) {
-                return dialect;
-            }
             throw new UnsupportedOperationException("I do not know which hibernate dialect to use with " +
-                    driver + ", use the property jpa.dialect in config file");
+                    driver + " and I cannot guess it, use the property jpa.dialect in config file");
         }
     }
+
 
     @Override
     public void onApplicationStop() {
@@ -232,7 +264,7 @@ public class JPAPlugin extends PlayPlugin {
 
     /**
      * initialize the JPA context and starts a JPA transaction
-     * 
+     *
      * @param readonly true for a readonly transaction
      */
     public static void startTx(boolean readonly) {
@@ -241,7 +273,7 @@ public class JPAPlugin extends PlayPlugin {
         }
         EntityManager manager = JPA.entityManagerFactory.createEntityManager();
         //if(Play.configuration.getProperty("future.bindJPAObjects", "false").equals("true")) {
-            manager.setFlushMode(FlushModeType.COMMIT);
+        manager.setFlushMode(FlushModeType.COMMIT);
         //}
         if (autoTxs) {
             manager.getTransaction().begin();
@@ -250,7 +282,8 @@ public class JPAPlugin extends PlayPlugin {
     }
 
     /**
-     * clear current JPA context and transaction 
+     * clear current JPA context and transaction
+     *
      * @param rollback shall current transaction be committed (false) or cancelled (true)
      */
     public static void closeTx(boolean rollback) {
@@ -279,7 +312,7 @@ public class JPAPlugin extends PlayPlugin {
                                     break;
                                 }
                             }
-                           throw new JPAException("Cannot commit", e);
+                            throw new JPAException("Cannot commit", e);
                         }
                     }
                 }
