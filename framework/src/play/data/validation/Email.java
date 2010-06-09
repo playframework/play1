9328a04fe8ae0ba84db78cce025ee58c98332a51
guillaume <guillaume@gbo-mac>
2010-06-09 19:49:28 +0200

    Fix Bug #591815

--- framework/src/play/data/validation/Email.java
+++ framework/src/play/data/validation/Email.java
@@ -14,6 +14,7 @@ import net.sf.oval.configuration.annotation.Constraint;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Constraint(checkWith = EmailCheck.class)
+
 public @interface Email {
 
     String message() default EmailCheck.mes;
