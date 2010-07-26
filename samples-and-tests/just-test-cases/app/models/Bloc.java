74dfeb5538c5e005aee127dcda9c422421079ef6
Erwan Loisant <erwan@coot>
2010-07-26 10:59:45 +0200

    Stop using deprecated annotations

--- samples-and-tests/just-test-cases/app/models/Bloc.java
+++ samples-and-tests/just-test-cases/app/models/Bloc.java
@@ -9,11 +9,10 @@ import java.util.*;
 @Entity
 public class Bloc extends Model {
 	
-	public String name;
+    public String name;
 
-    @org.hibernate.annotations.CollectionOfElements(fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.EAGER)
     @JoinTable(name = "Bloc_Criterias", joinColumns = @JoinColumn(name = "id"))
-    @org.hibernate.annotations.MapKey(columns = @Column(name = "propertyKey"))
     @Column(name = "propertyValue", nullable = false)
     @org.hibernate.annotations.Cascade(value = org.hibernate.annotations.CascadeType.ALL)
     public Map<String, String> criterias = new HashMap<String, String>();
