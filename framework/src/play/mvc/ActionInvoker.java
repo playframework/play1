0c346e5ef2ccf8e622ef74a0896899b75d4f2f6a
Quentin ADAM <waxzce@MacBook-Pro-de-waxzce.local>
2010-08-06 17:22:08 +0200

    add only param to @after

--- framework/src/play/mvc/ActionInvoker.java
+++ framework/src/play/mvc/ActionInvoker.java
@@ -227,7 +227,19 @@ public class ActionInvoker {
                 ControllerInstrumentation.stopActionCall();
                 for (Method after : afters) {
                     String[] unless = after.getAnnotation(After.class).unless();
+                    String[] only = after.getAnnotation(After.class).only();
                     boolean skip = false;
+                    for (String un : only) {
+                        if (!un.contains(".")) {
+                            un = after.getDeclaringClass().getName().substring(12) + "." + un;
+                        }
+                        if (un.equals(request.action)) {
+                            skip = false;
+                            break;
+                        } else {
+                            skip = true;
+                        }
+                    }
                     for (String un : unless) {
                         if (!un.contains(".")) {
                             un = after.getDeclaringClass().getName().substring(12) + "." + un;
