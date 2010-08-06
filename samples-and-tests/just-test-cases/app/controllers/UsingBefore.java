9b25b628e6b62476a99ea5ce74b60bc83d94d5c1
Quentin ADAM <waxzce@MacBook-Pro-de-waxzce.local>
2010-08-06 17:35:25 +0200

    some selenium tests for @before only params

--- samples-and-tests/just-test-cases/app/controllers/UsingBefore.java
+++ samples-and-tests/just-test-cases/app/controllers/UsingBefore.java
@@ -7,27 +7,34 @@ import java.util.*;
 
 public class UsingBefore extends Controller {
 
-    @Before(unless="a")
+    @Before(unless = {"a", "onlytest"})
     static void yop(String name) {
         renderText("Yop " + name);
     }
-    
+
     @Before
     public static void kiki() {
         System.out.println("X");
     }
-    
+
     public static void a(String name) {
         renderText("Youhou " + name);
     }
-    
+
     public static void b(String name) {
         renderText("Youhou " + name);
     }
-    
+
     static void isPrivate() {
         renderText("Oops");
     }
-    
-}
 
+    @Before(only = {"onlytest"})
+    static void beforeonlytest(String name) {
+        renderText("onlywork " + name);
+    }
+
+    public static void onlytest(String name) {
+        renderText("onlynotwork :  " + name);
+    }
+}
