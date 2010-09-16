46d627da7b6d40149ddb807fb91cb73945e3861d
Nicolas Leroux <nicolasleroux@Nicolas-Lerouxs-MacBook-Pro.local>
2010-09-16 22:20:14 +0200

    Missing file

--- framework/src/play/server/ssl/SslHttpServerPipelineFactory.java
+++ framework/src/play/server/ssl/SslHttpServerPipelineFactory.java
@@ -1,15 +1,21 @@
-package play.server;
+package play.server.ssl;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.handler.ssl.SslHandler;
 import org.jboss.netty.handler.stream.ChunkedWriteHandler;
+import play.Logger;
 import play.Play;
+import play.server.PlayHandler;
+import play.server.StreamChunkAggregator;
+
+import javax.net.ssl.SSLEngine;
 
 import static org.jboss.netty.channel.Channels.pipeline;
 
-public class HttpServerPipelineFactory implements ChannelPipelineFactory {
+public class SslHttpServerPipelineFactory implements ChannelPipelineFactory {
 
     public ChannelPipeline getPipeline() throws Exception {
 
@@ -17,13 +23,21 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
            
         ChannelPipeline pipeline = pipeline();
 
+        // Add SSL handler first to encrypt and decrypt everything.
+        SSLEngine engine =
+            SslHttpServerContextFactory.getServerContext().createSSLEngine();
+        engine.setUseClientMode(false);
+        engine.setNeedClientAuth(true);
+        engine.setWantClientAuth(true);
+        engine.setEnableSessionCreation(true);
+     
+        pipeline.addLast("ssl", new SslHandler(engine));
         pipeline.addLast("decoder", new HttpRequestDecoder());
         pipeline.addLast("aggregator", new StreamChunkAggregator(max));
         pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
 
-        pipeline.addLast("handler", new PlayHandler(false));
-
+        pipeline.addLast("handler", new PlayHandler(true));
 
         return pipeline;
     }
