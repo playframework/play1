f92a4ae67f71125243e05478e870fc1391eb3d86
Lawrence McAlpin <admin@lmcalpin.com>
2010-10-21 15:55:17 -0400

    [#319] add staticFile routing

--- framework/src/play/mvc/Router.java
+++ framework/src/play/mvc/Router.java
@@ -159,12 +159,16 @@ public class Router {
      */
     static void parse(VirtualFile routeFile, String prefix) {
         String fileAbsolutePath = routeFile.getRealFile().getAbsolutePath();
-        int lineNumber = 0;
         String content = routeFile.contentAsString();
         if (content.indexOf("${") > -1 || content.indexOf("#{") > -1) {
             // Mutable map needs to be passed in.
             content = TemplateLoader.load(routeFile).render(new HashMap<String, Object>(16));
         }
+        parse(content, prefix, fileAbsolutePath);
+    }
+    
+    static void parse(String content, String prefix, String fileAbsolutePath) {
+        int lineNumber = 0;
         for (String line : content.split("\n")) {
             lineNumber++;
             line = line.trim().replaceAll("\\s+", " ");
@@ -365,7 +369,7 @@ public class Router {
         }
         return reverse(file, absolute);
     }
-
+    
     public static ActionDefinition reverse(String action, Map<String, Object> args) {
         if (action.startsWith("controllers.")) {
             action = action.substring(12);
@@ -591,6 +595,7 @@ public class Router {
         Pattern actionPattern;
         List<String> actionArgs = new ArrayList<String>(3);
         String staticDir;
+        boolean staticFile;
         Pattern pattern;
         Pattern hostPattern;
         List<Arg> args = new ArrayList<Arg>(3);
@@ -607,8 +612,7 @@ public class Router {
         public void compute() {
             this.host = "";
             this.hostPattern = new Pattern(".*");
-            // staticDir
-            if (action.startsWith("staticDir:")) {
+            if (action.startsWith("staticDir:") || action.startsWith("staticFile:")) {
                 // Is there is a host argument, append it.
                 if (!path.startsWith("/")) {
                     String p = this.path;
@@ -623,12 +627,19 @@ public class Router {
                     Logger.warn("Static route only support GET method");
                     return;
                 }
+            }
+            // staticDir
+            if (action.startsWith("staticDir:")) {
                 if (!this.path.endsWith("/") && !this.path.equals("/")) {
                     Logger.warn("The path for a staticDir route must end with / (%s)", this);
                     this.path += "/";
                 }
                 this.pattern = new Pattern("^" + path + "({resource}.*)$");
                 this.staticDir = action.substring("staticDir:".length());
+            } else if (action.startsWith("staticFile:")) {
+                this.pattern = new Pattern("^" + path + "$");
+                this.staticFile = true;
+                this.staticDir = action.substring("staticFile:".length());
             } else {
                 // URL pattern
                 // Is there is a host argument, append it.
@@ -766,12 +777,16 @@ public class Router {
                 if (matcher.matches() && contains(accept) && hostMatches) {
                     // Static dir
                     if (staticDir != null) {
-                        String resource = matcher.group("resource");
+                        String resource = null;
+                        if (!staticFile) {
+                            resource = matcher.group("resource");
+                        }
                         try {
                             String root = new File(staticDir).getCanonicalPath();
-                            String child = new File(staticDir + "/" + resource).getCanonicalPath();
+                            String childResourceName = staticDir + (staticFile ? "" : "/" + resource);
+                            String child = new File(childResourceName).getCanonicalPath();
                             if (child.startsWith(root)) {
-                                throw new RenderStatic(staticDir + "/" + resource);
+                                throw new RenderStatic(childResourceName);
                             }
                         } catch (IOException e) {
                         }
