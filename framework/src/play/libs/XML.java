445c77dfa5d5ff57a3402dca13f23e83b732ce30
Erwan Loisant <eloisant@gmail.com>
2010-11-15 17:51:04 +0100

    [#416] Helper to check the xmldsig of an xml document

--- framework/src/play/libs/XML.java
+++ framework/src/play/libs/XML.java
@@ -1,19 +1,27 @@
 package play.libs;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.security.Key;
+import java.security.Provider;
 
+import javax.xml.crypto.KeySelector;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMValidateContext;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
-import java.io.ByteArrayInputStream;
-import org.xml.sax.InputSource;
 import play.Logger;
+
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 /**
  * XML utils
@@ -72,4 +80,28 @@ public class XML {
         }
         return null;
     }
+
+    /**
+     * Check the xmldsig signature of the XML document
+     * @param document the document to test
+     * @param publicKey the public key corresponding to the key pair the document was signed with
+     * @return true if a correct signature is present, false otherwise
+     */
+    public static boolean validSignature(Document document, Key publicKey) {
+        Node signatureNode =  document.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature").item(0);
+        KeySelector keySelector = KeySelector.singletonKeySelector(publicKey);
+
+        try {
+            String providerName = System.getProperty("jsr105Provider", "org.jcp.xml.dsig.internal.dom.XMLDSigRI");
+            XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM", (Provider) Class.forName(providerName).newInstance());
+            DOMValidateContext valContext = new DOMValidateContext(keySelector, signatureNode);
+
+            XMLSignature signature = fac.unmarshalXMLSignature(valContext);
+            return signature.validate(valContext);
+        } catch (Exception e) {
+            Logger.warn("Exception checking the signature: ", e);
+            return false;
+        }
+    }
+
 }
