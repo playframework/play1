08fb7014de731e84b3d09f852868f0bddc416505
Guillaume Bort <guillaume.bort@gmail.com>
2010-11-16 14:23:40 +0100

    [#335] Use a mutable list for Play.langs

--- framework/src/play/Play.java
+++ framework/src/play/Play.java
@@ -398,7 +398,7 @@ public class Play {
             Logger.setUp(logLevel);
 
             // Locales
-            langs = Arrays.asList(configuration.getProperty("application.langs", "").split(","));
+            langs = new ArrayList(Arrays.asList(configuration.getProperty("application.langs", "").split(",")));
             if (langs.size() == 1 && langs.get(0).trim().length() == 0) {
                 langs = new ArrayList<String>(16);
             }
