8415d5c31a8b2c61feffb996308a7da36ab015ea
Guillaume Bort <guillaume.bort@gmail.com>
2010-11-18 17:25:46 +0100

    [#353] Fix template compilation problem in some cases

--- framework/src/play/templates/GroovyTemplateCompiler.java
+++ framework/src/play/templates/GroovyTemplateCompiler.java
@@ -65,7 +65,7 @@ public class GroovyTemplateCompiler extends TemplateCompiler {
             source = source.replaceAll("new " + Pattern.quote(cName) + "(\\([^)]*\\))", "_('" + originalNames.get(cName) + "').newInstance$1");
             source = source.replaceAll("([a-zA-Z0-9.-_$]+)\\s+instanceof\\s+" + Pattern.quote(cName), "_('" + originalNames.get(cName).replace("$", "\\$") + "').isAssignableFrom($1.class)");
             source = source.replaceAll("([^.])" + Pattern.quote(cName) + ".class", "$1_('" + originalNames.get(cName) + "')");
-            source = source.replaceAll("([^'\".])" + Pattern.quote(cName) + "([^'\"])", "$1_('" + originalNames.get(cName).replace("$", "\\$") + "')$2");
+            source = source.replaceAll("([^'\".])" + Pattern.quote(cName) + "([.][^'\"])", "$1_('" + originalNames.get(cName).replace("$", "\\$") + "')$2");
         }
 
         return source;
