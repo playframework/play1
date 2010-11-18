a2488a47a38667ceeca1a34d1489632fa622c4d0
Guillaume Bort <guillaume.bort@gmail.com>
2010-11-18 17:25:46 +0100

    [#353] Fix template compilation problem in some cases

---
+++ samples-and-tests/just-test-cases/app/controllers/mapping/MappingRules.java
@@ -0,0 +1,17 @@
+package controllers.mapping;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class MappingRules extends Controller {
+
+    public static void index() {
+        render();
+    }
+    
+}
+
