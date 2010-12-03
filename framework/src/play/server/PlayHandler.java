d26adcd2233f6c34e6e00bc30bd5f6146c68af6f
Guillaume Bort <guillaume.bort@gmail.com>
2010-12-03 11:50:12 +0100

    Ensure that all streams used by play.libs.IO are correctly closed

--- framework/src/play/server/PlayHandler.java
+++ framework/src/play/server/PlayHandler.java
@@ -639,6 +639,7 @@ public class PlayHandler extends SimpleChannelUpstreamHandler {
         Logger.trace("serveStatic: begin");
         HttpResponse nettyResponse = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.valueOf(response.status));
         nettyResponse.setHeader("Server", signature);
+        RandomAccessFile raf = null;
         try {
             VirtualFile file = Play.getVirtualFile(renderStatic.file);
             if (file != null && file.exists() && file.isDirectory()) {
@@ -676,7 +677,6 @@ public class PlayHandler extends SimpleChannelUpstreamHandler {
                         }
                     } else {
 
-                        RandomAccessFile raf;
                         raf = new RandomAccessFile(localFile, "r");
                         long fileLength = raf.length();
 
@@ -716,6 +716,14 @@ public class PlayHandler extends SimpleChannelUpstreamHandler {
                 future.addListener(ChannelFutureListener.CLOSE);
             } catch (Exception ex) {
                 Logger.error(ez, "serveStatic for request %s", request.method + " " + request.url);
+            }
+        } finally {
+            if(raf != null) {
+                try {
+                    raf.close();
+                } catch(Exception ex) {
+                    //
+                }
             }
         }
         Logger.trace("serveStatic: end");
