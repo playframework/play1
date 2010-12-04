beafddac455fc4010485d87f7e27b9c8fb625214
Olivier Refalo <orefalo@yahoo.com>
2010-12-04 00:29:01 -0500

    @Phone @IPv4Address @IPv6Address validators with their respective test cases

---
+++ framework/src/play/data/validation/PhoneCheck.java
@@ -0,0 +1,31 @@
+package play.data.validation;
+
+import java.util.regex.Pattern;
+
+import net.sf.oval.Validator;
+import net.sf.oval.configuration.annotation.AbstractAnnotationCheck;
+import net.sf.oval.context.OValContext;
+import net.sf.oval.exception.OValException;
+
+public class PhoneCheck extends AbstractAnnotationCheck<Phone> {
+
+	final static String mes = "validation.phone";
+
+	static Pattern phonePattern = Pattern
+	.compile("^([\\+][0-9]{1,3}([ \\.\\-]))?([\\(]{1}[0-9]{2,6}[\\)])?([0-9 \\.\\-/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$");
+
+	@Override
+	public void configure(Phone phone) {
+		setMessage(phone.message());
+	}
+
+	@Override
+	public boolean isSatisfied(Object validatedObject, Object value, OValContext context, Validator validator)
+			throws OValException {
+		if (value == null || value.toString().length() == 0) {
+			return true;
+		}
+		return phonePattern.matcher(value.toString()).matches();
+	}
+
+}
---
+++ samples-and-tests/just-test-cases/test/IPValidationTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import play.data.validation.IPv4AddressCheck;
+import play.data.validation.IPv6AddressCheck;
+import play.test.UnitTest;
+
+public class IPValidationTest extends UnitTest {
+
+	@Test
+	public void ipv4() {
+		assertTrue(testv4("0.125.212.5", false));
+		assertTrue(testv4("999.524.251.55", false));
+		assertTrue(testv4("192.167.1.2222", false));
+		assertTrue(testv4("1922.167.1.222", false));
+		assertTrue(testv4("092.167.1.222", false));
+		assertTrue(testv4("192..1.222", false));
+		assertTrue(testv4("392.3.1.222", false));
+		assertTrue(testv4("127.0.0.1", true));
+		assertTrue(testv4("192.167.1.2", true));
+	}
+
+	@Test
+	public void ipv6() {
+		assertTrue(testv6("3ffe:1900:4545:3:200:f8ff:fe21:67cf", true));
+		assertTrue(testv6("fe80::200:f8ff:fe21:67cf", true));
+		assertTrue(testv6("fe80:0:0:0:200:f8ff:fe21:67cf", true));
+
+	}
+
+	private boolean testv4(String addr, boolean expected) {
+		IPv4AddressCheck check = new IPv4AddressCheck();
+		return expected == check.isSatisfied(null, addr, null, null);
+	}
+
+	private boolean testv6(String addr, boolean expected) {
+		IPv6AddressCheck check = new IPv6AddressCheck();
+		return expected == check.isSatisfied(null, addr, null, null);
+	}
+}
