0842f4cf40b6ba4346a64a3a3f8070acacf92de4
Olivier Refalo <orefalo@yahoo.com>
2010-12-07 17:51:38 -0800

    git is starting to bug me

--- framework/src/play/data/validation/PhoneCheck.java
+++ framework/src/play/data/validation/PhoneCheck.java
@@ -11,13 +11,8 @@ public class PhoneCheck extends AbstractAnnotationCheck<Phone> {
 
 	final static String mes = "validation.phone";
 
-<<<<<<< HEAD
 	static Pattern phonePattern = Pattern
 	.compile("^([\\+][0-9]{1,3}([ \\.\\-]))?([\\(]{1}[0-9]{2,6}[\\)])?([0-9 \\.\\-/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$");
-=======
-	static private Pattern phonePattern = Pattern
-			.compile("^([\\+][0-9]{1,3}([ \\.\\-])?)?([\\(]{1}[0-9]{3}[\\)])?([0-9A-Z \\.\\-]{1,32})((x|ext|extension)[ ]?[0-9]{1,4})?$");
->>>>>>> bf3829351cd50bea5e894840523857f6b309801f
 
 	@Override
 	public void configure(Phone phone) {
