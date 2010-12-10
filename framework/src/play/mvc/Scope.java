6006785d31e6760ac43e5e427651ce9909c6aa49
Newman Scott Hunter <scott@driftlogic.net>
2010-12-10 11:00:35 -0800

    Corrects params toString to accurately reflect available parameters.

--- framework/src/play/mvc/Scope.java
+++ framework/src/play/mvc/Scope.java
@@ -458,6 +458,10 @@ public class Scope {
 
         @Override
         public String toString() {
+            if( !requestIsParsed ){
+              checkAndParse();
+            }
+
             return data.toString();
         }
 
