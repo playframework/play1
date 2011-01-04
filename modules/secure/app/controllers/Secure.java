b538acfaf27005365f11a34945b365120b2c88bd
Erwan Loisant <eloisant@gmail.com>
2011-01-04 09:16:23 +0100

    trailing spaces

--- modules/secure/app/controllers/Secure.java
+++ modules/secure/app/controllers/Secure.java
@@ -36,7 +36,7 @@ public class Secure extends Controller {
             }
         }
     }
-    
+
     // ~~~ Login
 
     public static void login() throws Throwable {
@@ -87,9 +87,9 @@ public class Secure extends Controller {
         flash.success("secure.logout");
         login();
     }
-    
+
     // ~~~ Utils
-    
+
     static void redirectToOriginalURL() throws Throwable {
         Security.invoke("onAuthenticated");
         String url = flash.get("url");
