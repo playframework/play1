8a8f33910185703cb6d69c8b2386bdefd422b81b
Guillaume Bort <guillaume.bort@gmail.com>
2011-02-02 23:01:09 +0100

    Test cases for continuations

---
+++ samples-and-tests/just-test-cases/app/jobs/DoSomething.java
@@ -0,0 +1,20 @@
+package jobs;
+
+import play.jobs.*;
+import java.util.*;
+
+public class DoSomething extends Job<String> {
+    
+    long d;
+    
+    public DoSomething(long d) {
+        this.d = d;
+    }
+
+    public String doJobWithResult() throws Exception {
+        Thread.sleep(d);
+        return "DONE:" + d;
+    }
+    
+}
+
