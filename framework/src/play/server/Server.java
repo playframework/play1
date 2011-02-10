2ae9f1e5d88b16fbe0b9510f69701dd3b5ed3c39
aembleton <arthur@blerg.net>
2011-02-10 14:48:26 +0000

    Replaced tabs with 4 spaces

--- framework/src/play/server/Server.java
+++ framework/src/play/server/Server.java
@@ -18,8 +18,8 @@ import java.util.concurrent.Executors;
 
 public class Server {
 
-	public static int[] httpPorts;
-	public static int[] httpsPorts;
+    public static int[] httpPorts;
+    public static int[] httpsPorts;
 
     public Server(String[] args) {
 
@@ -29,19 +29,19 @@ public class Server {
         httpPorts = propertyToIntArray(args, "http.port", -1);
         httpsPorts = propertyToIntArray(args, "https.port", -1);
 
-		if (((httpPorts.length == 1) && (httpPorts[0] == -1)) && ((httpsPorts.length == 1) && (httpsPorts[0] == -1))) {
-			httpPorts = new int[] { 9000 };
-		}
+        if (((httpPorts.length == 1) && (httpPorts[0] == -1)) && ((httpsPorts.length == 1) && (httpsPorts[0] == -1))) {
+            httpPorts = new int[] { 9000 };
+        }
 
-		// look for any colliding ports
-		for (int httpPort : httpPorts) {
-			for (int httpsPort : httpsPorts) {
-				if (httpPort == httpsPort) {
-					Logger.error("Could not bind on https and http on the same port " + httpPort);
-					System.exit(-1);
-				}
-			}
-		}
+        // look for any colliding ports
+        for (int httpPort : httpPorts) {
+            for (int httpsPort : httpsPorts) {
+                if (httpPort == httpsPort) {
+                    Logger.error("Could not bind on https and http on the same port " + httpPort);
+                    System.exit(-1);
+                }
+            }
+        }
 
         InetAddress address = null;
         InetAddress secureAddress = null;
@@ -69,193 +69,193 @@ public class Server {
         ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
                 Executors.newCachedThreadPool(), Executors.newCachedThreadPool())
         );
-		if (httpPorts[0] != -1) {
-			bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
-			List<Integer> boundedPorts = new LinkedList<Integer>();
-			for (int httpPort : httpPorts) {
-				try {
-					bootstrap.bind(new InetSocketAddress(address, httpPort));
-					boundedPorts.add(httpPort);
-				} catch (ChannelException e) {
-					Logger.error("Could not bind on port " + httpPort, e);
-				}
-			}
-			bootstrap.setOption("child.tcpNoDelay", true);
+        if (httpPorts[0] != -1) {
+            bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+            List<Integer> boundedPorts = new LinkedList<Integer>();
+            for (int httpPort : httpPorts) {
+                try {
+                    bootstrap.bind(new InetSocketAddress(address, httpPort));
+                    boundedPorts.add(httpPort);
+                } catch (ChannelException e) {
+                    Logger.error("Could not bind on port " + httpPort, e);
+                }
+            }
+            bootstrap.setOption("child.tcpNoDelay", true);
 
-			String port = boundedPorts.size() > 1 ? "ports" : "port";
-			String boundedPortsString = integerListToCommaSeperatedString(boundedPorts, ", ");
+            String port = boundedPorts.size() > 1 ? "ports" : "port";
+            String boundedPortsString = integerListToCommaSeperatedString(boundedPorts, ", ");
 
-			if (Play.mode == Mode.DEV) {
-				if (address == null) {
-					Logger.info("Listening for HTTP on %s %s (Waiting a first request to start) ...", port, boundedPortsString);
-				} else {
-					Logger.info("Listening for HTTP at %s on %s %s (Waiting a first request to start) ...", address, port, boundedPortsString);
-				}
-			} else {
-				if (address == null) {
-					Logger.info("Listening for HTTP on %s %s ...", port, boundedPortsString);
-				} else {
-					Logger.info("Listening for HTTP at %s on %s %s  ...", httpPorts, address, port, boundedPortsString);
-				}
-			}
+            if (Play.mode == Mode.DEV) {
+                if (address == null) {
+                    Logger.info("Listening for HTTP on %s %s (Waiting a first request to start) ...", port, boundedPortsString);
+                } else {
+                    Logger.info("Listening for HTTP at %s on %s %s (Waiting a first request to start) ...", address, port, boundedPortsString);
+                }
+            } else {
+                if (address == null) {
+                    Logger.info("Listening for HTTP on %s %s ...", port, boundedPortsString);
+                } else {
+                    Logger.info("Listening for HTTP at %s on %s %s  ...", httpPorts, address, port, boundedPortsString);
+                }
+            }
 
-		}
+        }
 
         bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
                 Executors.newCachedThreadPool(), Executors.newCachedThreadPool())
         );
 
 
-		if ((httpsPorts.length > 0) && (httpsPorts[0] != -1)) {
-			bootstrap.setPipelineFactory(new SslHttpServerPipelineFactory());
-			List<Integer> boundedPorts = new LinkedList<Integer>();
-			for (int httpsPort : httpsPorts) {
-				try {
-					bootstrap.bind(new InetSocketAddress(secureAddress, httpsPort));
-					boundedPorts.add(httpsPort);
-				} catch (ChannelException e) {
-					Logger.error("Could not bind on port " + httpsPort, e);
-				}
-			}
-			bootstrap.setOption("child.tcpNoDelay", true);
+        if ((httpsPorts.length > 0) && (httpsPorts[0] != -1)) {
+            bootstrap.setPipelineFactory(new SslHttpServerPipelineFactory());
+            List<Integer> boundedPorts = new LinkedList<Integer>();
+            for (int httpsPort : httpsPorts) {
+                try {
+                    bootstrap.bind(new InetSocketAddress(secureAddress, httpsPort));
+                    boundedPorts.add(httpsPort);
+                } catch (ChannelException e) {
+                    Logger.error("Could not bind on port " + httpsPort, e);
+                }
+            }
+            bootstrap.setOption("child.tcpNoDelay", true);
 
-			String port = boundedPorts.size() > 1 ? "ports" : "port";
-			String boundedPortsString = integerListToCommaSeperatedString(boundedPorts, ", ");
+            String port = boundedPorts.size() > 1 ? "ports" : "port";
+            String boundedPortsString = integerListToCommaSeperatedString(boundedPorts, ", ");
 
-			if (Play.mode == Mode.DEV) {
-				if (secureAddress == null) {
-					Logger.info("Listening for HTTPS on %s %s (Waiting a first request to start) ...", port, boundedPortsString);
-				} else {
-					Logger.info("Listening for HTTPS at %s on %s %s (Waiting a first request to start) ...", secureAddress, port, boundedPortsString);
-				}
-			} else {
-				if (secureAddress == null) {
-					Logger.info("Listening for HTTPS on %s %s ...", port, boundedPortsString);
-				} else {
-					Logger.info("Listening for HTTPS at %s on %s %s  ...", httpPorts, secureAddress, port, boundedPortsString);
-				}
-			}
+            if (Play.mode == Mode.DEV) {
+                if (secureAddress == null) {
+                    Logger.info("Listening for HTTPS on %s %s (Waiting a first request to start) ...", port, boundedPortsString);
+                } else {
+                    Logger.info("Listening for HTTPS at %s on %s %s (Waiting a first request to start) ...", secureAddress, port, boundedPortsString);
+                }
+            } else {
+                if (secureAddress == null) {
+                    Logger.info("Listening for HTTPS on %s %s ...", port, boundedPortsString);
+                } else {
+                    Logger.info("Listening for HTTPS at %s on %s %s  ...", httpPorts, secureAddress, port, boundedPortsString);
+                }
+            }
 
-		}
+        }
     }
     
     /**
-	 * Generates a String containing the Integers held in the list separated by
-	 * whatever separator is past in. For example, for the list {1,2,3} you
-	 * could use the separator " -- " and get "1 -- 2 -- 3" returned.
-	 * 
-	 * @param integerList
-	 *            List of Integers that should be used to build the returned
-	 *            String
-	 * @param separator
-	 *            The String that is to be used to separate each Integer in the
-	 *            integerList.
-	 * @return A String containing the Integers in the integerList separated by
-	 *         the separator String. If integerList is null, then an empty
-	 *         String will be returned.
-	 */
-	private String integerListToCommaSeperatedString(List<Integer> integerList, String separator) {
-		StringBuffer sb = new StringBuffer();
+     * Generates a String containing the Integers held in the list separated by
+     * whatever separator is past in. For example, for the list {1,2,3} you
+     * could use the separator " -- " and get "1 -- 2 -- 3" returned.
+     * 
+     * @param integerList
+     *            List of Integers that should be used to build the returned
+     *            String
+     * @param separator
+     *            The String that is to be used to separate each Integer in the
+     *            integerList.
+     * @return A String containing the Integers in the integerList separated by
+     *         the separator String. If integerList is null, then an empty
+     *         String will be returned.
+     */
+    private String integerListToCommaSeperatedString(List<Integer> integerList, String separator) {
+        StringBuffer sb = new StringBuffer();
 
-		if (integerList != null) {
-			for (int i = 0; i < integerList.size(); i++) {
-				if (i > 0) {
-					// not the first element, so add a separator
-					sb.append(separator);
-				}
-				sb.append(integerList.get(i));
-			}
-		}
+        if (integerList != null) {
+            for (int i = 0; i < integerList.size(); i++) {
+                if (i > 0) {
+                    // not the first element, so add a separator
+                    sb.append(separator);
+                }
+                sb.append(integerList.get(i));
+            }
+        }
 
-		return sb.toString();
-	}
+        return sb.toString();
+    }
 
-	/**
-	 * Converts the value of a property into an array of primitive ints. The
-	 * property must have a comma separated list of ints. If there is only one
-	 * value, and so isn't comma separated then only one value will be returned
-	 * in the array. If no property is found then the defaultValue will be used.
-	 * 
-	 * @param propertyName
-	 *            The name of the property to lookup
-	 * @param defaultValue
-	 *            The value to use if no value for the property can be found.
-	 * @return An array of ints that are extracted from the comma separated
-	 *         value of the property with name equal to propertyName.
-	 */
-	private int[] propertyToIntArray(String[] args, String propertyName, int defaultValue) {
-		final Properties p = Play.configuration;
+    /**
+     * Converts the value of a property into an array of primitive ints. The
+     * property must have a comma separated list of ints. If there is only one
+     * value, and so isn't comma separated then only one value will be returned
+     * in the array. If no property is found then the defaultValue will be used.
+     * 
+     * @param propertyName
+     *            The name of the property to lookup
+     * @param defaultValue
+     *            The value to use if no value for the property can be found.
+     * @return An array of ints that are extracted from the comma separated
+     *         value of the property with name equal to propertyName.
+     */
+    private int[] propertyToIntArray(String[] args, String propertyName, int defaultValue) {
+        final Properties p = Play.configuration;
 
-		if (propertyName == null) {
-			// no property name, so just return a -1 value
-			return new int[] { -1 };
-		}
+        if (propertyName == null) {
+            // no property name, so just return a -1 value
+            return new int[] { -1 };
+        }
 
-		String value = getOpt(args, propertyName, p.getProperty(propertyName, String.valueOf(defaultValue)));
+        String value = getOpt(args, propertyName, p.getProperty(propertyName, String.valueOf(defaultValue)));
 
-		String[] valueArray = value.split(",");
+        String[] valueArray = value.split(",");
 
-		int[] intArray = stringArrayToIntArray(valueArray);
+        int[] intArray = stringArrayToIntArray(valueArray);
 
-		if ((intArray == null) || (intArray.length < 1)) {
-			// intArray has not been set, so use the default value
-			intArray = new int[] { defaultValue };
-		}
+        if ((intArray == null) || (intArray.length < 1)) {
+            // intArray has not been set, so use the default value
+            intArray = new int[] { defaultValue };
+        }
 
-		return intArray;
-	}
+        return intArray;
+    }
 
-	/**
-	 * Converts an array of String objects into an array of ints. If a String
-	 * does not parse into an Int then an error is logged and we move onto the
-	 * next element in the array.
-	 * 
-	 * @param stringArray
-	 *            The array of strings to parse into Integer objects.
-	 * @return An array of itns containing the parsed value of each element in
-	 *         the stringArray.
-	 */
-	private int[] stringArrayToIntArray(String[] stringArray) {
-		List<Integer> intList = new LinkedList<Integer>();
+    /**
+     * Converts an array of String objects into an array of ints. If a String
+     * does not parse into an Int then an error is logged and we move onto the
+     * next element in the array.
+     * 
+     * @param stringArray
+     *            The array of strings to parse into Integer objects.
+     * @return An array of itns containing the parsed value of each element in
+     *         the stringArray.
+     */
+    private int[] stringArrayToIntArray(String[] stringArray) {
+        List<Integer> intList = new LinkedList<Integer>();
 
-		if (stringArray != null) {
-			for (String element : stringArray) {
-				try {
-					Integer intValue = Integer.parseInt(element.trim());
-					intList.add(intValue);
-				} catch (NumberFormatException e) {
-					// failed to parse
-					Logger.error("%s is not a valid integer, this will be ignored.", element);
-				}
-			}
-		}
+        if (stringArray != null) {
+            for (String element : stringArray) {
+                try {
+                    Integer intValue = Integer.parseInt(element.trim());
+                    intList.add(intValue);
+                } catch (NumberFormatException e) {
+                    // failed to parse
+                    Logger.error("%s is not a valid integer, this will be ignored.", element);
+                }
+            }
+        }
 
-		return toIntArray(intList);
-	}
+        return toIntArray(intList);
+    }
 
-	/**
-	 * Converts an array of Integer objects to an array of int primitives
-	 * 
-	 * @param intList
-	 *            List of Integer objects to copy
-	 * 
-	 * @return Array of int primitives. If intList is null then an empty array
-	 *         will be returned.
-	 */
-	private int[] toIntArray(List<Integer> intList) {
+    /**
+     * Converts an array of Integer objects to an array of int primitives
+     * 
+     * @param intList
+     *            List of Integer objects to copy
+     * 
+     * @return Array of int primitives. If intList is null then an empty array
+     *         will be returned.
+     */
+    private int[] toIntArray(List<Integer> intList) {
 
-		if (intList == null) {
-			return new int[] {};
-		}
+        if (intList == null) {
+            return new int[] {};
+        }
 
-		int[] intArray = new int[intList.size()];
+        int[] intArray = new int[intList.size()];
 
-		for (int i = 0; i < intList.size(); i++) {
-			intArray[i] = intList.get(i);
-		}
+        for (int i = 0; i < intList.size(); i++) {
+            intArray[i] = intList.get(i);
+        }
 
-		return intArray;
-	}
+        return intArray;
+    }
 
     private String getOpt(String[] args, String arg, String defaultValue) {
         String s = "--" + arg + "=";
