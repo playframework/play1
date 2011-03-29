4217802fdd657511babff40b500af0f4cf2e8dee
Morten Kjetland <mbk@kjetland.com>
2011-03-29 21:17:10 +0200

    [#121] PlayPlugin.compareTo() now has natural orderings consistent with equals()

--- framework/src/play/PlayPlugin.java
+++ framework/src/play/PlayPlugin.java
@@ -271,7 +271,27 @@ public abstract class PlayPlugin implements Comparable<PlayPlugin> {
 
     // ~~~~~
     public int compareTo(PlayPlugin o) {
-        return (index < o.index ? -1 : (index == o.index ? 0 : 1));
+        int res = index < o.index ? -1 : (index == o.index ? 0 : 1);
+        if (res!=0) {
+            return res;
+        }
+
+        // index is equal in both plugins.
+        // sort on classtype to get consistent order
+        res = this.getClass().getName().compareTo(o.getClass().getName());
+        if (res != 0 ) {
+            // classnames where different
+            return res;
+        }
+
+        // identical classnames.
+        // sort on instance to get consistent order.
+        // We only return 0 (equal) if both identityHashCode are identical
+        // which is only the case if both this and other are the same object instance.
+        // This is consistent with equals() when no special equals-method is implemented.
+        int thisHashCode = System.identityHashCode(this);
+        int otherHashCode = System.identityHashCode(o);
+        return (thisHashCode < otherHashCode ? -1 : (thisHashCode == otherHashCode ? 0 : 1));
     }
 
     public String overrideTemplateSource(BaseTemplate template, String source) {
