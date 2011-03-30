d863a943a9cfca0ad724213331a4fa41cc7458a5
Nicolas Leroux <leroux.nicolas@gmail.com>
2011-03-30 22:05:57 +0200

    [#693]

--- framework/src/play/data/binding/Unbinder.java
+++ framework/src/play/data/binding/Unbinder.java
@@ -1,36 +1,38 @@
 package play.data.binding;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import play.Logger;
 import play.Play;
 import play.data.binding.types.DateBinder;
+import play.libs.I18N;
+import play.utils.Utils;
 
 /**
  * Try to unbind an object to a Map<String,String>
  */
 public class Unbinder {
 
-    public static void unBind(Map<String, Object> result, Object src, String name) {
+    public static void unBind(Map<String, Object> result, Object src, String name, Annotation[] annotations) {
         if (src == null) {
             return;
         }
-        unBind(result, src, src.getClass(), name);
+        unBind(result, src, src.getClass(), name, annotations);
     }
 
-    private static void unBind(Map<String, Object> result, Object src, Class<?> srcClazz, String name) {
+    private static void unBind(Map<String, Object> result, Object src, Class<?> srcClazz, String name, Annotation[] annotations) {
         Map<String, Object> r = Play.pluginCollection.unBind(src, name);
         if (r != null) {
             result.putAll(r);
             return;
         }
-        
+
         if (isDirect(srcClazz) || src == null) {
             if (!result.containsKey(name)) {
                 result.put(name, src != null ? src.toString() : null);
@@ -45,7 +47,7 @@ public class Unbinder {
             Class<?> clazz = src.getClass().getComponentType();
             int size = Array.getLength(src);
             for (int i = 0; i < size; i++) {
-                unBind(result, Array.get(src, i), clazz, name);
+                unBind(result, Array.get(src, i), clazz, name, annotations);
             }
         } else if (Collection.class.isAssignableFrom(src.getClass())) {
             if (Map.class.isAssignableFrom(src.getClass())) {
@@ -55,11 +57,40 @@ public class Unbinder {
                 List<Object> objects = new ArrayList<Object>();
                 result.put(name, objects);
                 for (Object object : c) {
-                    unBind(result, object, object.getClass(), name);
+                    unBind(result, object, object.getClass(), name, annotations);
                 }
             }
-        } else if (Date.class.isAssignableFrom(src.getClass())) {
-            result.put(name, new SimpleDateFormat(DateBinder.ISO8601).format((Date) src));
+        } else if (Date.class.isAssignableFrom(src.getClass()) || Calendar.class.isAssignableFrom(src.getClass())) {
+            // We should use the @As annotation if there is one
+            // Get the date format from the controller
+            boolean isAsAnnotation = false;
+            try {
+                if (annotations != null) {
+                    for (Annotation annotation : annotations) {
+                        if (annotation.annotationType().equals(As.class)) {
+                            if (Calendar.class.isAssignableFrom(src.getClass())) {
+                                result.put(name, new SimpleDateFormat(((As) annotation).value()[0]).format(((Calendar) src).getTime()));
+                            } else {
+                                result.put(name, new SimpleDateFormat(((As) annotation).value()[0]).format((Date) src));
+                            }
+                            isAsAnnotation = true;
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                // Ignore
+            }
+
+
+            if (!isAsAnnotation) {
+                // We want to use that one so when redirecting it looks ok. We could as well use the DateBinder.ISO8601 but the url looks terrible
+                if (Calendar.class.isAssignableFrom(src.getClass())) {
+                    result.put(name, new SimpleDateFormat(I18N.getDateFormat()).format(((Calendar) src).getTime()));
+                } else {
+                    result.put(name, new SimpleDateFormat(I18N.getDateFormat()).format((Date) src));
+                }
+            }
         } else {
             Field[] fields = src.getClass().getDeclaredFields();
             for (Field field : fields) {
@@ -67,7 +98,7 @@ public class Unbinder {
                 boolean oldAcc = field.isAccessible();
                 field.setAccessible(true);
                 try {
-                    unBind(result, field.get(src), field.getType(), newName);
+                    unBind(result, field.get(src), field.getType(), newName, annotations);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Object" + src.getClass() + " won't unbind field " + field.getName(), e);
                 } catch (IllegalArgumentException e) {
