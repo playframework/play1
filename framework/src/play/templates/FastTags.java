31a17c9edabeb38f4dbb1b397b8e1d3bf1066708
Ludovico Fischer <livrerie@gmail.com>
2011-04-01 22:12:22 +0200

    [#694] Make jsAction usable in object literal.

--- framework/src/play/templates/FastTags.java
+++ framework/src/play/templates/FastTags.java
@@ -54,7 +54,7 @@ public class FastTags {
     }
 
     public static void _jsAction(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
-        out.println("function(options) {var pattern = '" + args.get("arg").toString().replace("&amp;", "&") + "'; for(key in options) { pattern = pattern.replace(':'+key, options[key]); } return pattern };");
+        out.println("function(options) {var pattern = '" + args.get("arg").toString().replace("&amp;", "&") + "'; for(key in options) { pattern = pattern.replace(':'+key, options[key]); } return pattern }");
     }
 
     public static void _authenticityToken(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
