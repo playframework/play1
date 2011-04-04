038f69149cc8f993ab265ccbe47e971ac614bf86
Guillaume Bort <guillaume.bort@gmail.com>
2011-04-04 16:59:15 +0200

    Remove useless Python parts

--- python/Lib/site-packages/_spe/doc/home.py
+++
@@ -1,2 +0,0 @@
-import webbrowser
-webbrowser.open('http://www.stani.be/python/spe/blog')
--- python/Lib/site-packages/_spe/plugins/kiki/Kiki.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-@ECHO Use this file to start the program under Windows.
-SET PATH
-start pythonw -OO kiki.py
-exit
