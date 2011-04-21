3c2b71ea9818addc2252c55496443d05b40b56fc
Stephane Godbillon <sgo@zenexity.com>
2011-04-21 10:51:44 +0200

    [#802] Replace the runtime local variables tracker by a static one

--- framework/src/play/CorePlugin.java
+++ framework/src/play/CorePlugin.java
@@ -16,7 +16,7 @@ import play.classloading.ApplicationClasses.ApplicationClass;
 import play.classloading.enhancers.ContinuationEnhancer;
 import play.classloading.enhancers.ControllersEnhancer;
 import play.classloading.enhancers.Enhancer;
-import play.classloading.enhancers.LocalvariablesNamesEnhancer;
+import play.classloading.enhancers.LVEnhancer;
 import play.classloading.enhancers.MailerEnhancer;
 import play.classloading.enhancers.PropertiesEnhancer;
 import play.classloading.enhancers.SigEnhancer;
@@ -285,10 +285,10 @@ public class CorePlugin extends PlayPlugin {
         Class<?>[] enhancers = new Class[]{
             SigEnhancer.class,
             ControllersEnhancer.class,
+            LVEnhancer.class,
             ContinuationEnhancer.class,
             MailerEnhancer.class,
-            PropertiesEnhancer.class,
-            LocalvariablesNamesEnhancer.class
+            PropertiesEnhancer.class
         };
         for (Class<?> enhancer : enhancers) {
             try {
