playframework/play1

commit 98243e7a5719b2fe4416b50b157620848f9d9e5e

Prevent variable referencing before assignment

framework/pym/play/commands/modulesrepo.py
@@ -396,6 +396,7 @@ def install(app, args, env):
 def add(app, args, env):
     app.check()
 
+    m = None
     try:
         optlist, args = getopt.getopt(args, '', ['module='])
         for o, a in optlist:

commit 462d6298234de7839873bbc9758ccaedb325fd0e

Fix regression introduced by 98243e7

framework/pym/play/commands/modulesrepo.py
@@ -336,7 +336,7 @@ def install(app, args, env):
     for mod in modules_list:
         if mod['name'] == module:
             for v in mod['versions']:
-                if version is not None and v['isDefault']:
+                if version is None and v['isDefault']:
                     print '~ Will install %s-%s' % (module, v['version'])
                     print '~ This module is compatible with: %s' % v['matches']
                     ok = raw_input('~ Do you want to install this version (y/n)? ')