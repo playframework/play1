7f69111d2f59c66414a4917f3272367f75e4a94d
jeanbaptiste <jeanbaptiste.claramonte@gmail.com>
2011-05-13 11:15:41 +0200

    a small fix to avoid having null|null in db for Blob type

--- framework/src/play/db/jpa/Blob.java
+++ framework/src/play/db/jpa/Blob.java
@@ -58,7 +58,7 @@ public class Blob implements BinaryField, UserType {
     }
 
     public boolean exists() {
-        return UUID != null && getFile().exists();
+        return UUID != null && !UUID.equalsIgnoreCase("null") && getFile().exists();
     }
 
     public File getFile() {
@@ -95,7 +95,7 @@ public class Blob implements BinaryField, UserType {
     }
 
     public void nullSafeSet(PreparedStatement ps, Object o, int i) throws HibernateException, SQLException {
-        if(o != null) {
+        if(o != null && ((Blob)o).UUID != null && !((Blob)o).UUID.equalsIgnoreCase("null")) {
             ps.setString(i, ((Blob)o).UUID + "|" + ((Blob)o).type);
         } else {
             ps.setNull(i, Types.VARCHAR);
