2b0bf66212a0a32b28fe1daa65e1639d16dd48c6
David CHAU <dchau@OSX10026.local>
2011-05-16 10:47:33 -0400

    [#834] implement version management in the war command

--- framework/pym/play/commands/war.py
+++ framework/pym/play/commands/war.py
@@ -2,6 +2,7 @@ import sys
 import os
 import getopt
 import shutil
+from datetime import datetime
 
 import play.commands.precompile
 from play.utils import *
@@ -24,12 +25,14 @@ def execute(**kargs):
     war_exclusion_list = []
     try:
         optlist, args = getopt.getopt(args, 'o:', ['output=', 'zip','exclude='])
+        version = app.readConf('application.version')
+        if not version:
+            version = datetime.now().strftime("%Y%d%d%I%M%S")
         for o, a in optlist:
             if o in ('-o', '--output'):
-                war_path = os.path.normpath(os.path.abspath(a.format(version=app.readConf('application.version'))))
+                war_path = os.path.normpath(os.path.abspath(a.format(version)))
         if not war_path:
             war_path = app.readConf('application.name')
-            version = app.readConf('application.version')
             if version:
                 war_path = "%s-%s" % (war_path, version)
             war_path = os.path.normpath(os.path.abspath(war_path))
