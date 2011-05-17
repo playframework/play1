ff1c753148b32f274f4052de75b26c47336e106a
Stephane Epardaud <stephane@epardaud.fr>
2011-05-17 17:20:10 +0200

    [MultiKeys]: Support multiple keys in Model.Factory, Model.Property and JPAModelLoader

--- framework/src/play/db/Model.java
+++ framework/src/play/db/Model.java
@@ -3,6 +3,8 @@ package play.db;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+
 import play.Play;
 import play.exceptions.UnexpectedException;
 
@@ -21,6 +23,7 @@ public interface Model {
         public boolean isMultiple;
         public boolean isRelation;
         public boolean isGenerated;
+        public boolean isKey;
         public Class<?> relationType;
         public Choices choices;
 
@@ -34,15 +37,37 @@ public interface Model {
 
     public static interface Factory {
 
+    	/**
+    	 * Returns the list of properties for this factory's type.
+    	 */
+    	public List<Model.Property> listProperties();
+    	/**
+    	 * Returns the list of key properties for this factory's type.
+    	 */
+        public List<Model.Property> listKeys();
+        /**
+         * @deprecated this only works for single non-composite keys. It will throw an exception in every other case. Use listKeys().
+         */
+        @Deprecated
         public String keyName();
+        /**
+         * Returns the type of key. For a single key it will be the key's field's type. For a composite key this will be the type
+         * of the composite key (as specified by @IdClass)
+         */
         public Class<?> keyType();
+        /**
+         * Returns the key value. For a single key it will return the key's field. For a composite key this will return an instance of the type
+         * of the composite key (as specified by @IdClass)
+         */
         public Object keyValue(Model m);
+        /**
+         * Makes a key valid for this factory's type, with all the given components of this key taken from a map.
+         */
+        public Object makeKey(Map<String, Object> id);
         public Model findById(Object id);
         public List<Model> fetch(int offset, int length, String orderBy, String orderDirection, List<String> properties, String keywords, String where);
         public Long count(List<String> properties, String keywords, String where);
         public void deleteAll();
-        public List<Model.Property> listProperties();
-
     }
 
     public static class Manager {
