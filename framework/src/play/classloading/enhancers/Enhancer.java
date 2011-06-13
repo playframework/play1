79f1639a641a4a043bd6f242e25f8dce2688a534
Guillaume Bort <guillaume.bort@gmail.com>
2011-06-13 13:07:45 +0200

    Fix dynamic enhancer in precompiled mode

--- framework/src/play/classloading/enhancers/Enhancer.java
+++ framework/src/play/classloading/enhancers/Enhancer.java
@@ -1,8 +1,10 @@
 package play.classloading.enhancers;
 
+import java.io.File;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.FileInputStream;
 import java.lang.annotation.Annotation;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,6 +20,7 @@ import javassist.NotFoundException;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.annotation.MemberValue;
 import play.Play;
+import play.Logger;
 import play.classloading.ApplicationClasses.ApplicationClass;
 
 /**
@@ -57,6 +60,14 @@ public abstract class Enhancer {
     public static class ApplicationClassesClasspath implements ClassPath {
 
         public InputStream openClassfile(String className) throws NotFoundException {
+            if(Play.usePrecompiled) {
+                try {
+                    File file = Play.getFile("precompiled/java/" + className.replace(".", "/") + ".class");
+                    return new FileInputStream(file);
+                } catch(Exception e) {
+                    Logger.error("Missing class %s", className);
+                }
+            }
             return new ByteArrayInputStream(Play.classes.getApplicationClass(className).enhancedByteCode);
         }
 
