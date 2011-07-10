33ac6220bef10787854501ca89ada2d8d69b1a44
Mark Piper <revbingo@gmail.com>
2011-07-10 23:12:31 +0100

    [#966] No need to use storeOrRestore...()

--- framework/src/play/mvc/Controller.java
+++ framework/src/play/mvc/Controller.java
@@ -955,7 +955,7 @@ public class Controller implements ControllerSupport {
     protected static void await(int millis, F.Action0 callback) {
         Request.current().isNew = false;
         Request.current().args.put(ActionInvoker.A, callback);
-        storeOrRestoreDataStateForContinuations(null);
+        Request.current().args.put(ActionInvoker.CONTINUATIONS_STORE_RENDER_ARGS, Scope.RenderArgs.current());
         throw new Suspend(millis);
     }
 
@@ -1037,7 +1037,7 @@ public class Controller implements ControllerSupport {
         Request.current().isNew = false;
         Request.current().args.put(ActionInvoker.F, future);
         Request.current().args.put(ActionInvoker.A, callback);
-        storeOrRestoreDataStateForContinuations(null);
+        Request.current().args.put(ActionInvoker.CONTINUATIONS_STORE_RENDER_ARGS, Scope.RenderArgs.current());
         throw new Suspend(future);
     }
 
