dc1a5d124f0461aa9eb54d3ef01299665166eb24
Erwan Loisant <eloisant@gmail.com>
2011-08-09 10:50:47 +0200

    [#1023] Allow JVM arguments in the evolutions command

--- framework/pym/play/commands/evolutions.py
+++ framework/pym/play/commands/evolutions.py
@@ -41,9 +41,8 @@ def execute(**kargs):
     if args.count('--jpda'):
         print "~ Waiting for JPDA client to continue"
         add_options.extend(['-Xdebug', '-Xrunjdwp:transport=dt_socket,address=8888,server=y,suspend=y'])
-    for arg in args:
-        if arg.startswith("-D"):
-            add_options.append(arg)
+        args.remove('--jpda')
+    add_options.extend(args)
 
     java_cmd = [app.java_path()] + add_options + ['-classpath', app.cp_args(), 'play.db.Evolutions']
 
