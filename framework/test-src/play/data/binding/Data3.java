898c026470c1d84eb2ef0fef37187470f763b7c5
Oskari Porkka <oskari.porkka@gmail.com>
2011-09-05 22:55:03 +0300

    Added Map<String, String> binding tests

---
+++ framework/test-src/play/data/binding/Data3.java
@@ -0,0 +1,10 @@
+package play.data.binding;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Data3 {
+    public String a;
+    public Map<String, String> map = new HashMap<String, String>();
+}
