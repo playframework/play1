34625e40e8ebe10b23985f5b6e05f48b0cfabc78
Morten Kjetland <mbk@kjetland.com>
2011-10-09 20:40:00 +0200

    [#1097] added fixCaseForHttpHeader() which helps using the correct casing for http-headers

--- framework/src/play/utils/HTTP.java
+++ framework/src/play/utils/HTTP.java
@@ -1,5 +1,13 @@
 package play.utils;
 
+import play.libs.IO;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HTTP {
 
     public static class ContentTypeWithEncoding {
@@ -30,5 +38,42 @@ public class HTTP {
             return new ContentTypeWithEncoding(_contentType, _encoding);
         }
 
+    }
+
+    private static final Map<String, String> lower2UppercaseHttpHeaders = initLower2UppercaseHttpHeaders();
+
+    private static Map<String, String> initLower2UppercaseHttpHeaders() {
+        Map<String, String> map = new HashMap<String, String>();
+
+        String path = "/play/utils/http_headers.properties";
+        InputStream in = HTTP.class.getResourceAsStream(path);
+        if (in == null) {
+            throw new RuntimeException("Error reading " + path);
+        }
+        List<String> lines = IO.readLines( in );
+        for (String line : lines) {
+            line = line.trim();
+            if ( !line.startsWith("#")) {
+                map.put(line.toLowerCase(), line);
+            }
+        }
+
+        return Collections.unmodifiableMap(map);
+    }
+
+    /**
+     * Use this method to make sure you have the correct caseing of a http header name.
+     * eg: fixes 'content-type' to 'Content-Type'
+     */
+    public static String fixCaseForHttpHeader( String headerName) {
+        if (headerName == null) {
+            return null;
+        }
+        String correctCase = lower2UppercaseHttpHeaders.get(headerName.toLowerCase());
+        if ( correctCase != null) {
+            return correctCase;
+        }
+        // Didn't find it - return it as it is
+        return headerName;
     }
 }
