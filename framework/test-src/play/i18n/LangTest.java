3e1da481cb2f876b654d359e04bc5e01f992e501
sao <sao@imis.ch>
2011-11-22 09:44:44 +0100

    Added LangTest

---
+++ framework/test-src/play/i18n/LangTest.java
@@ -0,0 +1,30 @@
+/*
+ * (C) 2011 IMIS group AG, Zurich, Switzerland.
+ */
+package play.i18n;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+public class LangTest
+{
+    @Test
+    public void testGetLocaleLanguage()
+    {
+        Locale locale = Lang.getLocale("de");
+        assertThat(locale.getCountry(), is(""));
+        assertThat(locale.getLanguage(), is("de"));
+    }
+    
+    @Test
+    public void testGetLocaleCountry()
+    {
+        Locale locale = Lang.getLocale("en_US");
+        assertThat(locale.getCountry(), is("US"));
+        assertThat(locale.getLanguage(), is("en"));
+    }
+}
