f45fe8f6887be6e31bc0a2e3c106002f7ae87935
Nicolas Leroux <leroux.nicolas@gmail.com>
2011-12-01 10:22:37 +0100

    [#1232] Fix JPABinder to bind multiple ids

--- samples-and-tests/just-test-cases/test/YamlTest.java
+++ samples-and-tests/just-test-cases/test/YamlTest.java
@@ -10,7 +10,9 @@ public class YamlTest extends UnitTest {
         Fixtures.load("yamlTestData.yml");
         YamlModel ym = YamlModel.all().first();
         assertEquals("Morten", ym.name);
-        
+
+        assertEquals(DataWithCompositeKey.all().fetch().size(), 2);
+
         //check binary
         assertEquals("This String is stored in yaml file using base64", new String(ym.binaryData));
         
