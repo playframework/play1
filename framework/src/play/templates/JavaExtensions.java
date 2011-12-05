ea94ce40beeed01d9c9d3bf584579ecad6e6af5c
Rich Hong <hong.rich@gmail.com>
2011-12-04 19:30:42 -0500

    [#1295] Don't require a request in JavaExtensions.urlEncode

--- framework/src/play/templates/JavaExtensions.java
+++ framework/src/play/templates/JavaExtensions.java
@@ -237,7 +237,11 @@ public class JavaExtensions {
 
     public static String urlEncode(String entity) {
         try {
-            return URLEncoder.encode(entity, Http.Response.current().encoding);
+            String encoding = play.Play.defaultWebEncoding;
+            if (Http.Response.current() != null) {
+                encoding = Http.Response.current().encoding;
+            }
+            return URLEncoder.encode(entity, encoding);
         } catch (UnsupportedEncodingException e) {
             Logger.error(e, entity);
         }
