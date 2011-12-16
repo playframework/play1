4119c27c9d0f5aafb3ff8f46e256f5d4ea52ea0a
Andreas Simon <a.simon@quagilis.de>
2011-12-16 16:52:37 +0100

    Deleted several @override annotations that corrupted Java 5 compatibility

--- framework/src/play/deps/DependenciesManager.java
+++ framework/src/play/deps/DependenciesManager.java
@@ -61,7 +61,6 @@ public class DependenciesManager {
     
     final FileFilter dirsToTrim = new FileFilter() {
     
-        @Override
         public boolean accept(File file) {
             return file.isDirectory() && isDirToTrim(file.getName());
         }
