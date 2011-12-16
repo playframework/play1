fc8a2ec3b773886693837be5b7f112c9dcd5c0e1
Andreas Simon <a.simon@quagilis.de>
2011-12-16 16:53:10 +0100

    Removed @Override annotation from interface implementations on play/server/ServletWrapperTest

--- framework/test-src/play/server/ServletWrapperTest.java
+++ framework/test-src/play/server/ServletWrapperTest.java
@@ -73,48 +73,39 @@ public class ServletWrapperTest {
 			this.headers = headers;
 		}
 
-		@Override
 		public String getHeader(String key) {
 			return headers.get(key);
 		}
 
-		@Override
 		public String getAuthType() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getContextPath() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Cookie[] getCookies() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public long getDateHeader(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Enumeration getHeaderNames() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Enumeration getHeaders(final String arg0) {
 			
 			return new Enumeration<Object>() {
 				String element = headers.get(arg0);
 
-				@Override
 				public boolean hasMoreElements() {
 					return element != null;
 				}
 
-				@Override
 				public Object nextElement() {
 					Object current = element;
 					current = null;
@@ -123,237 +114,190 @@ public class ServletWrapperTest {
 			}; 
 		}
 
-		@Override
 		public int getIntHeader(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getMethod() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getPathInfo() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getPathTranslated() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getQueryString() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRemoteUser() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRequestURI() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public StringBuffer getRequestURL() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRequestedSessionId() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getServletPath() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public HttpSession getSession() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public HttpSession getSession(boolean arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Principal getUserPrincipal() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isRequestedSessionIdFromCookie() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isRequestedSessionIdFromURL() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isRequestedSessionIdFromUrl() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isRequestedSessionIdValid() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isUserInRole(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Object getAttribute(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Enumeration getAttributeNames() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getCharacterEncoding() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public int getContentLength() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getContentType() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public ServletInputStream getInputStream() throws IOException {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getLocalAddr() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getLocalName() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public int getLocalPort() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Locale getLocale() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Enumeration getLocales() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getParameter(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Map getParameterMap() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public Enumeration getParameterNames() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String[] getParameterValues(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getProtocol() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public BufferedReader getReader() throws IOException {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRealPath(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRemoteAddr() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getRemoteHost() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public int getRemotePort() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public RequestDispatcher getRequestDispatcher(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getScheme() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public String getServerName() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public int getServerPort() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public boolean isSecure() {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public void removeAttribute(String arg0) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public void setAttribute(String arg0, Object arg1) {
 			throw new RuntimeException("Method not implemented");
 		}
 
-		@Override
 		public void setCharacterEncoding(String arg0)
 				throws UnsupportedEncodingException {
 			throw new RuntimeException("Method not implemented");
