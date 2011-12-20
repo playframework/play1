5d146a82898a9e35211552425a34924be877045a
Nicolas Leroux <leroux.nicolas@gmail.com>
2011-12-20 11:28:45 +0100

    [#1206] - Data binding for collection of more than 10 elements

--- framework/src/play/data/binding/Binder.java
+++ framework/src/play/data/binding/Binder.java
@@ -423,7 +423,16 @@ public abstract class Binder {
             List l = (List) r;
 
             // must get all indexes and sort them so we add items in correct order.
-            Set<String> indexes = new TreeSet<String>(paramNode.getAllChildrenKeys());
+            Set<String> indexes = new TreeSet<String>(new Comparator<String>() {
+                public int compare(String arg0, String arg1) {
+                    try {
+                        return Integer.parseInt(arg0) - Integer.parseInt(arg1);
+                    } catch (NumberFormatException e) {
+                        return arg0.compareTo(arg1);
+                    }
+                }
+            });
+            indexes.addAll(paramNode.getAllChildrenKeys());
 
             // get each value in correct order with index
 
