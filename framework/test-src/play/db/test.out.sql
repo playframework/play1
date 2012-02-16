784bd956ea4a6e1188ec462102db5b773cd7c968
Wilhansen Li <wil@byimplication.com>
2012-02-16 18:38:40 +0800

    [#997] Fix indiscriminate splitting based on ';' in evolutions.

---
+++ framework/test-src/play/db/test.out.sql
@@ -0,0 +1,23 @@
+CREATE TABLE test(name varchar);===
+-- CREATE TABLE test2(name varchar);
+CREATE TABLE test3(
+	name varchar,
+	quant int,
+	cost real);===
+CREATE TABLE 'tes;t4'(name varchar);===
+CREATE FUNCTION test5() RETURNS VOID AS
+$$
+BEGIN
+	SELECT * FROM test;
+	RETURN;
+END;
+$$
+LANGUAGE plpgsql;===
+CREATE FUNCTION test6() RETURNS VOID AS
+$function$
+BEGIN
+	SELECT * FROM $1$test3$1$;
+	RETURN;
+END;
+$function$
+LANGUAGE plpgsql;
