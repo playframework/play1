408d3f1e0d2083dd0e571c4460342ffed3112745
Brian Lawler <bplawler@gmail.com>
2012-03-15 13:57:07 -0700

    [#1441] Re-apply (and improve upon) my original patch for this issues.  Now we take into account the other fix that detects and empty request.domain string.  I applied this fix and tested it on 1.2.4, on a local guest VM (hence the need for non-localhost considerations).

--- framework/src/play/db/DBPlugin.java
+++ framework/src/play/db/DBPlugin.java
@@ -27,17 +27,25 @@ public class DBPlugin extends PlayPlugin {
     public boolean rawInvocation(Request request, Response response) throws Exception {
         if (Play.mode.isDev() && request.path.equals("/@db")) {
             response.status = Http.StatusCode.MOVED;
+            String serverOptions[] = new String[] { };
 
             // For H2 embeded database, we'll also start the Web console
             if (h2Server != null) {
                 h2Server.stop();
             }
-            h2Server = org.h2.tools.Server.createWebServer();
-            h2Server.start();
 
             String domain = request.domain;
-            if (domain.equals(""))
-              domain = "localhost";
+            if (domain.equals("")) {
+                domain = "localhost";
+            }
+
+            if (!domain.equals("localhost")) {
+                serverOptions = new String[] {"-webAllowOthers"};
+            }
+            
+            h2Server = org.h2.tools.Server.createWebServer(serverOptions);
+            h2Server.start();
+
             response.setHeader("Location", "http://" + domain + ":8082/");
             return true;
         }
