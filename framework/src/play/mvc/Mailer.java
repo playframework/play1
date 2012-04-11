6329bce3cd1dcb176addca8662998702b2579a00
chungonn <chungonn@greenfossil.com>
2012-04-12 00:21:56 +0800

    Added Mailer#attachDataSource

--- framework/src/play/mvc/Mailer.java
+++ framework/src/play/mvc/Mailer.java
@@ -21,6 +21,10 @@ import play.templates.Template;
 import play.templates.TemplateLoader;
 
 import javax.mail.internet.InternetAddress;
+import play.libs.F;
+import play.libs.F.T4;
+import javax.mail.util.ByteArrayDataSource;
+import javax.activation.DataSource;
 
 /**
  * Application mailer support
@@ -97,6 +101,28 @@ public class Mailer implements LocalVariablesSupport {
         attachmentsList.addAll(Arrays.asList(attachments));
         infos.set(map);
     }
+
+   @SuppressWarnings("unchecked")
+   public static void attachDataSource(DataSource dataSource, String name, String description, String disposition){
+    	
+        HashMap<String, Object> map = infos.get();
+        if (map == null) {
+            throw new UnexpectedException("Mailer not instrumented ?");
+        }
+        List<T4<DataSource, String, String, String>> datasourceList = (List<T4<DataSource, String, String, String>>) map.get("datasources");
+        if (datasourceList == null) {
+            datasourceList = new ArrayList<T4<DataSource, String, String, String>>();
+            map.put("datasources", datasourceList);
+        }
+        datasourceList.add(F.T4(dataSource, name, description, disposition));
+        infos.set(map);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static void attachDataSource(DataSource dataSource, String name, String description){
+       attachDataSource(dataSource, name, description, EmailAttachment.ATTACHMENT);
+    }
+    
 
     public static void setContentType(String contentType) {
         HashMap<String, Object> map = infos.get();
@@ -245,7 +271,7 @@ public class Mailer implements LocalVariablesSupport {
             final Object replyTo = infos.get().get("replyTo");
 
             Email email = null;
-            if (infos.get().get("attachments") == null) {
+            if (infos.get().get("attachments") == null && infos.get().get("datasources") == null ) {
                 if (StringUtils.isEmpty(bodyHtml)) {
                     email = new SimpleEmail();
                     email.setMsg(bodyText);
@@ -272,9 +298,20 @@ public class Mailer implements LocalVariablesSupport {
                 }
                 MultiPartEmail multiPartEmail = (MultiPartEmail) email;
                 List<EmailAttachment> objectList = (List<EmailAttachment>) infos.get().get("attachments");
-                for (EmailAttachment object : objectList) {
-                    multiPartEmail.attach(object);
-                }
+                
+	       		if(objectList != null){
+	                  for (EmailAttachment object : objectList) {
+	                      multiPartEmail.attach(object);
+	                  }
+				}
+		
+			    //Handle DataSource
+			    List<T4<DataSource, String, String, String>> datasourceList = (List<T4<DataSource, String, String, String>>) infos.get().get("datasources");
+				if(datasourceList != null){
+			          for(T4<DataSource, String, String, String> ds : datasourceList){
+			             multiPartEmail.attach(ds._1, ds._2, ds._3, ds._4);
+			          }
+				}
             }
             email.setCharset("utf-8");
 
