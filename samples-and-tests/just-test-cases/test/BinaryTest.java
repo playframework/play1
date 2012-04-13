b88f283ec3591b607bf2288e3c7afeeaf72c48a5
Yee Fan <yeefan@kaisquare.com>
2012-04-14 03:01:06 +0800

    [#1489] Make sure renderBinary closes the input stream.

--- samples-and-tests/just-test-cases/test/BinaryTest.java
+++ samples-and-tests/just-test-cases/test/BinaryTest.java
@@ -16,151 +16,143 @@ import play.test.Fixtures;
 import play.test.FunctionalTest;
 import controllers.Binary;
 
-
 public class BinaryTest extends FunctionalTest {
-	
 
-	@Before
-	public void setUp() {
-		Fixtures.deleteAll(); // see Bug #491403
-		Fixtures.deleteDirectory("attachments");
-		URL deleteURL = reverse(); {
-			Binary.deleteAll();
-		}		
-		Response deletedResponse = GET(deleteURL);
-		assertStatus(200, deletedResponse);
-	}
-	
-	@Test
-	public void testUploadSomething() {
-		URL imageURL = reverse(); {
-			Binary.showAvatar(1l);
-		}
-		Response getResponse = GET(imageURL);
-		assertStatus(404, getResponse);
-		
-		URL url = reverse(); {
-			Binary.save(null);
-		}
-		Map<String,String> parameters= new HashMap<String,String>();
-		parameters.put("user.username", "username");
-		Map<String, File> files= new HashMap<String, File>();
-		File f = Play.getFile("test/fond1.png");
-		assertTrue(f.exists());
-		files.put("user.avatar", f);
-		Response uploadResponse = POST(url, parameters, files);
-		assertStatus(302, uploadResponse);
-		String id = uploadResponse.getHeader("Location").split("=")[1];
-		imageURL = reverse(); {
-			Binary.showAvatar(new Long(id));
-		}
-		getResponse = GET(imageURL);
-		assertStatus(200, getResponse);
-	}
+    @Before
+    public void setUp() {
+        Fixtures.deleteAll(); // see Bug #491403
+        Fixtures.deleteDirectory("attachments");
+        URL deleteURL = reverse(); {
+            Binary.deleteAll();
+        }
+        Response deletedResponse = GET(deleteURL);
+        assertStatus(200, deletedResponse);
+    }
+    
+    @Test
+    public void testUploadSomething() {
+        URL imageURL = reverse(); {
+            Binary.showAvatar(1l);
+        }
+        Response getResponse = GET(imageURL);
+        assertStatus(404, getResponse);
+        
+        URL url = reverse(); {
+            Binary.save(null);
+        }
+        Map<String,String> parameters= new HashMap<String,String>();
+        parameters.put("user.username", "username");
+        Map<String, File> files= new HashMap<String, File>();
+        File f = Play.getFile("test/fond1.png");
+        assertTrue(f.exists());
+        files.put("user.avatar", f);
+        Response uploadResponse = POST(url, parameters, files);
+        assertStatus(302, uploadResponse);
+        String id = uploadResponse.getHeader("Location").split("=")[1];
+        imageURL = reverse(); {
+            Binary.showAvatar(new Long(id));
+        }
+        getResponse = GET(imageURL);
+        assertStatus(200, getResponse);
+    }
 
     @Test
-	public void testUploadBigFile() {
+    public void testUploadBigFile() {
 
-		Map<String,String> parameters= new HashMap<String,String>();
+        Map<String,String> parameters= new HashMap<String,String>();
 
-		Map<String, File> files= new HashMap<String, File>();
-		File file = Play.getFile("test/winie.jpg");
-		assertTrue(file.exists());
-		files.put("file", file);
-		Response uploadResponse = POST("/Binary/uploadFile", parameters, files);
+        Map<String, File> files= new HashMap<String, File>();
+        File file = Play.getFile("test/winie.jpg");
+        assertTrue(file.exists());
+        files.put("file", file);
+        Response uploadResponse = POST("/Binary/uploadFile", parameters, files);
 
         assertStatus(200, uploadResponse);
 
         String size = uploadResponse.getHeader("Content-Length");
 
-	    assertEquals("Size does not match", "1366949", size);
-	}
-
-
+        assertEquals("Size does not match", "1366949", size);
+    }
 
     @Test
-	public void testUploadBigFile2() {
+    public void testUploadBigFile2() {
 
-		Map<String,String> parameters= new HashMap<String,String>();
+        Map<String,String> parameters= new HashMap<String,String>();
 
-		Map<String, File> files= new HashMap<String, File>();
-		File file = Play.getFile("test/winie.jpg");
-		assertTrue(file.exists());
+        Map<String, File> files= new HashMap<String, File>();
+        File file = Play.getFile("test/winie.jpg");
+        assertTrue(file.exists());
 
-		files.put("upload", file);
-		Response uploadResponse = POST("/Binary/upload", parameters, files);
+        files.put("upload", file);
+        Response uploadResponse = POST("/Binary/upload", parameters, files);
 
         assertStatus(200, uploadResponse);
 
         String size = uploadResponse.getHeader("Content-Length");
 
-	    assertEquals("Size does not match", "1366949", size);
-	}
+        assertEquals("Size does not match", "1366949", size);
+    }
 
     @Test
-	public void testUploadSmallFile() {
+    public void testUploadSmallFile() {
 
-		Map<String,String> parameters= new HashMap<String,String>();
+        Map<String,String> parameters= new HashMap<String,String>();
 
-		Map<String, File> files= new HashMap<String, File>();
-		File file = Play.getFile("test/angel.gif");
-		assertTrue(file.exists());
-		files.put("file", file);
-		Response uploadResponse = POST("/Binary/uploadFile", parameters, files);
+        Map<String, File> files= new HashMap<String, File>();
+        File file = Play.getFile("test/angel.gif");
+        assertTrue(file.exists());
+        files.put("file", file);
+        Response uploadResponse = POST("/Binary/uploadFile", parameters, files);
 
         assertStatus(200, uploadResponse);
 
         String size = uploadResponse.getHeader("Content-Length");
 
-	    assertEquals("Size does not match", "2440", size);
-	}
-
-
+        assertEquals("Size does not match", "2440", size);
+    }
 
     @Test
-	public void testUploadSmallFile2() {
+    public void testUploadSmallFile2() {
 
-		Map<String,String> parameters= new HashMap<String,String>();
+        Map<String,String> parameters= new HashMap<String,String>();
 
-		Map<String, File> files= new HashMap<String, File>();
-		File file = Play.getFile("test/angel.gif");
-		assertTrue(file.exists());
+        Map<String, File> files= new HashMap<String, File>();
+        File file = Play.getFile("test/angel.gif");
+        assertTrue(file.exists());
 
-		files.put("upload", file);
-		Response uploadResponse = POST("/Binary/upload", parameters, files);
+        files.put("upload", file);
+        Response uploadResponse = POST("/Binary/upload", parameters, files);
 
         assertStatus(200, uploadResponse);
 
         String size = uploadResponse.getHeader("Content-Length");
 
-	    assertEquals("Size does not match", "2440", size);
-	}
-
+        assertEquals("Size does not match", "2440", size);
+    }
 
 //  TODO: Missing possibility to upload multiple files at once
 //  See: http://play.lighthouseapp.com/projects/57987-play-framework/tickets/472-functionaltest-and-ws-client-library-dont-allow-upload-of-multiple-file#ticket-472-2 
-//  @Test
-//	public void testMultipleUpload() {
+//    @Test
+//    public void testMultipleUpload() {
 //
-//		Map<String,String> parameters= new HashMap<String,String>();
+//        Map<String,String> parameters= new HashMap<String,String>();
 //
-//		Map<String, File[]> files= new HashMap<String, File[]>();
-//		File file1 = Play.getFile("test/angel.gif");
-//		assertTrue(file1.exists());
+//        Map<String, File[]> files= new HashMap<String, File[]>();
+//        File file1 = Play.getFile("test/angel.gif");
+//        assertTrue(file1.exists());
 //
 //        File file2 = Play.getFile("test/winie   .gif");
 //        assertTrue(file1.exists());
 //
-//		files.put("upload", new File[] {file1, file2 });
-//		Response uploadResponse = POST("/Binary/uploadMultiple", parameters, files);
+//        files.put("upload", new File[] {file1, file2 });
+//        Response uploadResponse = POST("/Binary/uploadMultiple", parameters, files);
 //
 //        assertStatus(200, uploadResponse);
 //
 //        String size = uploadResponse.getHeader("Content-Length");
 //
-//	    assertEquals("Size does not match", "2440", size);
-//	}
-
+//        assertEquals("Size does not match", "2440", size);
+//    }
 
     @Test
     public void testGetBinaryWithCustomContentType() {
@@ -168,5 +160,24 @@ public class BinaryTest extends FunctionalTest {
         assertIsOk(response);
         assertContentType("custom/contentType", response);
     }
-    
+
+    // Tests to check whether input streams to renderBinary are closed.
+
+    @Test
+    public void testGetEmptyBinary() {
+        Response response = GET("/binary/getEmptyBinary");
+        assertIsOk(response);
+        assertTrue(Binary.emptyInputStreamClosed);
+    }
+
+    @Test
+    public void testGetErrorBinary() {
+        try {
+            GET("/binary/getErrorBinary");
+            fail();
+        }
+        catch (Exception e) {
+        }
+        assertTrue(Binary.errorInputStreamClosed);
+    }
 }
