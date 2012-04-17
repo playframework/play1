afe0113fbbbea996fcb3a66c6f3578a9abb09bf3
Jaime Hablutzel <hablutzel1@gmail.com>
2012-04-17 10:58:47 -0500

    Corrected NPE for patch: IllegalAccessException shouldn't be ignored from Binder

--- framework/src/play/data/binding/Binder.java
+++ framework/src/play/data/binding/Binder.java
@@ -199,7 +199,7 @@ public abstract class Binder {
         } catch (BinderException e) {
             throw e; // allow binder to throw an exception that propagates
         } catch (Exception e) {
-            if (IllegalAccessException.class.isAssignableFrom(e.getClass()) || e.getCause() != null || IllegalAccessException.class.isAssignableFrom(e.getCause().getClass())){
+            if (IllegalAccessException.class.isAssignableFrom(e.getClass()) || ( e.getCause() != null && IllegalAccessException.class.isAssignableFrom(e.getCause().getClass()))){
                 throw new RuntimeException(e);
             }
             Validation.addError(paramNode.getOriginalKey(), "validation.invalid");
