3248c3ab4fdef1b7b74cd1199c0467afc0b9c74c
Nicolas Leroux <leroux.nicolas@gmail.com>
2012-05-06 14:47:00 +0200

    [#765] delete all model fixture  with  mapped super class

--- framework/src/play/test/Fixtures.java
+++ framework/src/play/test/Fixtures.java
@@ -37,6 +37,8 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.persistence.Entity;
+
 public class Fixtures {
 
     static Pattern keyPattern = Pattern.compile("([^(]+)\\(([^)]+)\\)");
@@ -95,7 +97,9 @@ public class Fixtures {
     public static void deleteAllModels() {
         List<Class<? extends Model>> classes = new ArrayList<Class<? extends Model>>();
         for (ApplicationClasses.ApplicationClass c : Play.classes.getAssignableClasses(Model.class)) {
-            classes.add((Class<? extends Model>)c.javaClass);
+		   if( c.javaClass.isAnnotationPresent(Entity.class) ) {
+		       classes.add((Class<? extends Model>)c.javaClass);
+		    }
         }
         disableForeignKeyConstraints();
         Fixtures.delete(classes);
