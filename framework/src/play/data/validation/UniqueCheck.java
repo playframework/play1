73f9f98483bd483228fb042808d8eb1e897ca1e7
Nicolas Leroux <leroux.nicolas@gmail.com>
2012-05-07 14:57:44 +0200

    [#1502] JPA/Hibernate warns about use of deprecated positional parameter

--- framework/src/play/data/validation/UniqueCheck.java
+++ framework/src/play/data/validation/UniqueCheck.java
@@ -69,6 +69,7 @@ public class UniqueCheck extends AbstractAnnotationCheck<Unique> {
         final Object[] values = new Object[isUpdate ? propertyNames.length + 1 :
                 propertyNames.length];
         final Class clazz = validatedObject.getClass();
+		int index = 1;
         for (int i = 0; i < propertyNames.length; i++) {
             Field field = getField(clazz, propertyNames[i]);
             field.setAccessible(true);
@@ -80,11 +81,11 @@ public class UniqueCheck extends AbstractAnnotationCheck<Unique> {
             if (i > 0) {
                 jpql.append(" And ");
             }
-            jpql.append("o.").append(propertyNames[i]).append(" = ? ");
+            jpql.append("o.").append(propertyNames[i]).append(" = ?" + String.valueOf(index++) + " ");
         }
         if (isUpdate) {
             values[propertyNames.length] = keyValue;
-            jpql.append(" and o.").append(keyProperty).append(" <>  ?");
+            jpql.append(" and o.").append(keyProperty).append(" <>  ?" + String.valueOf(index++) + " ");
         }
         return JPQL.instance.count(entityName, jpql.toString(), values) == 0L;
     }
