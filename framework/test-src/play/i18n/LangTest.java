9cb18404461971933c5a917203977824f7d23db1
Nicolas Leroux <leroux.nicolas@gmail.com>
2012-05-10 12:27:10 -0700

    Merge pull request #509 from nemecec/i18n
    use realistic values for accept-language header

--- framework/test-src/play/i18n/LangTest.java
+++ framework/test-src/play/i18n/LangTest.java
@@ -1,15 +1,16 @@
 package play.i18n;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Locale;
+
 import org.junit.Test;
+
 import play.Play;
 import play.PlayBuilder;
 import play.mvc.Http;
 import play.test.FunctionalTest;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.fest.assertions.Assertions.*;
 
 public class LangTest {
 
@@ -48,7 +49,7 @@ public class LangTest {
     @Test
     public void testGet() {
         new PlayBuilder().build();
-        Play.langs = Arrays.asList("no", "en", "en_uk", "fr");
+        Play.langs = Arrays.asList("no", "en", "en_GB", "fr");
         Lang.current.set(null);
 
         Http.Response.current.set( new Http.Response());
@@ -61,44 +62,50 @@ public class LangTest {
         Http.Request req = FunctionalTest.newRequest();
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("no");
+        assertLocale(new Locale("no"));
 
         // check only with accept-language,  without cookie value
         req = FunctionalTest.newRequest();
         req.headers.put("accept-language", new Http.Header("accept-language", "x"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("no");
+        assertLocale(new Locale("no"));
 
         req = FunctionalTest.newRequest();
         req.headers.put("accept-language", new Http.Header("accept-language", "no"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("no");
+        assertLocale(new Locale("no"));
 
         req = FunctionalTest.newRequest();
         req.headers.put("accept-language", new Http.Header("accept-language", "en"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en");
+        assertLocale(new Locale("en"));
 
         req = FunctionalTest.newRequest();
         req.headers.put("accept-language", new Http.Header("accept-language", "x,en"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en");
+        assertLocale(new Locale("en"));
 
         req = FunctionalTest.newRequest();
-        req.headers.put("accept-language", new Http.Header("accept-language", "en_uk"));
+        req.headers.put("accept-language", new Http.Header("accept-language", "en-GB"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en_uk");
+        assertLocale(new Locale("en", "GB"));
 
         req = FunctionalTest.newRequest();
-        req.headers.put("accept-language", new Http.Header("accept-language", "x,en_uk"));
+        req.headers.put("accept-language", new Http.Header("accept-language", "x,en-GB"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en_uk");
+        assertLocale(new Locale("en", "GB"));
+
+        req = FunctionalTest.newRequest();
+        req.headers.put("accept-language", new Http.Header("accept-language", "x,en-US"));
+        Http.Request.current.set(req);
+        Lang.current.set(null);
+        assertLocale(new Locale("en"));
 
         // check with cookie value
 
@@ -111,7 +118,7 @@ public class LangTest {
         req.headers.put("accept-language", new Http.Header("accept-language", "en"));
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en");
+        assertLocale(new Locale("en"));
 
         cookie = new Http.Cookie();
         cookie.name = "PLAY_LANG";
@@ -119,7 +126,7 @@ public class LangTest {
         req.cookies.put(cookie.name, cookie);
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en");
+        assertLocale(new Locale("en"));
 
         cookie = new Http.Cookie();
         cookie.name = "PLAY_LANG";
@@ -127,16 +134,21 @@ public class LangTest {
         req.cookies.put(cookie.name, cookie);
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en");
+        assertLocale(new Locale("en"));
 
         cookie = new Http.Cookie();
         cookie.name = "PLAY_LANG";
-        cookie.value = "en_uk";
+        cookie.value = "en_GB";
         req.cookies.put(cookie.name, cookie);
         Http.Request.current.set(req);
         Lang.current.set(null);
-        assertThat(Lang.get()).isEqualTo("en_uk");
+        assertLocale(new Locale("en", "GB"));
 
 
+    }
+
+    private void assertLocale(Locale locale) {
+      assertThat(Lang.get()).isEqualTo(locale.toString());
+      assertThat(Lang.getLocale()).isEqualTo(locale);
     }
 }
