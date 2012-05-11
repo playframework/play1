f20af9fe8edfb85a06895ddf944b44be67f742df
Erem Boto <ehboto@gmail.com>
2012-05-10 17:05:31 -0700

    Fixes NPE in MimeTypes.getContentType. See issue #1519

---
+++ framework/test-src/play/libs/MimeTypesTest.java
@@ -0,0 +1,46 @@
+package play.libs;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.mvc.Http.Response;
+
+
+/**
+ * Tests for {@link MimeTypes} class.
+ */
+public class MimeTypesTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void contentTypeShouldReturnResponseCharsetWhenAvailable() throws Exception {
+        String oldEncoding = Response.current().encoding;
+        try {
+            Response.current().encoding = "my-response-encoding";
+            assertEquals("text/xml; charset=my-response-encoding",
+                         MimeTypes.getContentType("test.xml"));
+        }
+        finally {
+            Response.current().encoding = oldEncoding;
+        }
+    }
+
+    @Test
+    public void contentTypeShouldReturnDefaultCharsetInAbsenceOfResponse() throws Exception {
+        Response originalResponse = Response.current();
+        try {
+            Response.current.set(null);
+            assertEquals("text/xml; charset=" + play.Play.defaultWebEncoding,
+                         MimeTypes.getContentType("test.xml"));
+        }
+        finally {
+            Response.current.set(originalResponse);
+        }
+    }
+
+}
