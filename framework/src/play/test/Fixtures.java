d609f5789d20fcf8bca198a3ae7f0b5348eb638b
Schaaf, Martin <mschaaf@datameer.com>
2012-08-02 10:07:17 +0300

    Add check for existens of model file to load

--- framework/src/play/test/Fixtures.java
+++ framework/src/play/test/Fixtures.java
@@ -199,7 +199,7 @@ public class Fixtures {
                     break;
                 }
             }
-            if (yamlFile == null) {
+            if (yamlFile == null || !yamlFile.exists()) {
                 throw new RuntimeException("Cannot load fixture " + name + ", the file was not found");
             }
 
