b44d2ba309fe43ffc203d6446afe92705901d736
Nicolas Leroux <leroux.nicolas@gmail.com>
2012-12-29 05:43:29 -0800

    Merge pull request #536 from nemecec/master
    ticket 1586: avoid NPE if template is not found and applicationClass is null

--- framework/src/play/mvc/Controller.java
+++ framework/src/play/mvc/Controller.java
@@ -18,6 +18,7 @@ import play.Invoker.Suspend;
 import play.Logger;
 import play.Play;
 import play.classloading.ApplicationClasses;
+import play.classloading.ApplicationClasses.ApplicationClass;
 import play.classloading.enhancers.ContinuationEnhancer;
 import play.classloading.enhancers.ControllersEnhancer.ControllerInstrumentation;
 import play.classloading.enhancers.ControllersEnhancer.ControllerSupport;
@@ -670,10 +671,12 @@ public class Controller implements ControllerSupport, LocalVariablesSupport {
             }
             StackTraceElement element = PlayException.getInterestingStrackTraceElement(ex);
             if (element != null) {
-                throw new TemplateNotFoundException(templateName, Play.classes.getApplicationClass(element.getClassName()), element.getLineNumber());
-            } else {
-                throw ex;
+                ApplicationClass applicationClass = Play.classes.getApplicationClass(element.getClassName());
+                if (applicationClass != null) {
+                    throw new TemplateNotFoundException(templateName, applicationClass, element.getLineNumber());
+                }
             }
+            throw ex;
         }
     }
 
