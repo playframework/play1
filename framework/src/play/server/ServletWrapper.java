42fd788ebece7814296af79ff2c54c488752edd8
Alex <alexandre.chatiron@gmail.com>
2013-05-21 12:56:14 +0800

    [#1182] Close Input file is not closed in ServletWrapper

--- framework/src/play/server/ServletWrapper.java
+++ framework/src/play/server/ServletWrapper.java
@@ -504,15 +504,26 @@ public class ServletWrapper extends HttpServlet implements ServletContextListene
 
     }
 
-    private void copyStream(HttpServletResponse servletResponse, InputStream is) throws IOException {
-        OutputStream os = servletResponse.getOutputStream();
-        byte[] buffer = new byte[8096];
-        int read = 0;
-        while ((read = is.read(buffer)) > 0) {
-            os.write(buffer, 0, read);
+    private void copyStream(HttpServletResponse servletResponse, InputStream is) throws IOException {        
+        if (servletResponse != null && is != null) {
+            try {
+                OutputStream os = servletResponse.getOutputStream();
+                byte[] buffer = new byte[8096];
+                int read = 0;
+                while ((read = is.read(buffer)) > 0) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            } catch (IOException ex) {
+                throw ex;
+            }finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    Logger.error("Cannot close input stream.", e);
+                }
+            }
         }
-        os.flush();
-        is.close();
     }
 
     public class ServletInvocation extends Invoker.DirectInvocation {
