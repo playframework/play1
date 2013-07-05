a606f47e583f2ad154a6af76ce0c8885f8fc715d
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2013-07-05 16:33:29 +0800

    [#1625] Fix include file method [#1625] Add localized included messages in test-case

--- framework/src/play/i18n/MessagesPlugin.java
+++ framework/src/play/i18n/MessagesPlugin.java
@@ -22,66 +22,68 @@ import play.vfs.VirtualFile;
 public class MessagesPlugin extends PlayPlugin {
 
     static Long lastLoading = 0L;
-    
+
     private static List<String> includeMessageFilenames = new ArrayList<String>();
 
     @Override
     public void onApplicationStart() {
-	includeMessageFilenames.clear();
+        includeMessageFilenames.clear();
         Messages.defaults = new Properties();
         try {
             File message = new File(Play.frameworkPath, "resources/messages");
             Messages.defaults.putAll(read(message));
-        } catch(Exception e) {
+        } catch (Exception e) {
             Logger.warn("Defaults messsages file missing");
         }
-        for(VirtualFile module : Play.modules.values()) {
+        for (VirtualFile module : Play.modules.values()) {
             VirtualFile messages = module.child("conf/messages");
-            if(messages != null && messages.exists() && !messages.isDirectory()) {
-                Messages.defaults.putAll(read(messages)); 
+            if (messages != null && messages.exists()
+                    && !messages.isDirectory()) {
+                Messages.defaults.putAll(read(messages));
             }
         }
         VirtualFile appDM = Play.getVirtualFile("conf/messages");
-        if(appDM != null && appDM.exists() && !appDM.isDirectory()) {
+        if (appDM != null && appDM.exists() && !appDM.isDirectory()) {
             Messages.defaults.putAll(read(appDM));
         }
         for (String locale : Play.langs) {
             Properties properties = new Properties();
-            for(VirtualFile module : Play.modules.values()) {
+            for (VirtualFile module : Play.modules.values()) {
                 VirtualFile messages = module.child("conf/messages." + locale);
-                if(messages != null && messages.exists()  && !messages.isDirectory()) {
-                    properties.putAll(read(messages)); 
+                if (messages != null && messages.exists()
+                        && !messages.isDirectory()) {
+                    properties.putAll(read(messages));
                 }
             }
             VirtualFile appM = Play.getVirtualFile("conf/messages." + locale);
-            if(appM != null && appM.exists()  && !appM.isDirectory()) {
+            if (appM != null && appM.exists() && !appM.isDirectory()) {
                 properties.putAll(read(appM));
             } else {
                 Logger.warn("Messages file missing for locale %s", locale);
-            }     
+            }
             Messages.locales.put(locale, properties);
         }
         lastLoading = System.currentTimeMillis();
     }
 
     static Properties read(VirtualFile vf) {
-	if(vf != null){
-	    return read(vf.getRealFile());
-	}
-	return null;
+        if (vf != null) {
+            return read(vf.getRealFile());
+        }
+        return null;
     }
-            
-    static Properties read(File file) {   
-	Properties propsFromFile=null;
+
+    static Properties read(File file) {
+        Properties propsFromFile = null;
         if (file != null && !file.isDirectory()) {
             InputStream inStream = null;
             try {
-        	inStream = new FileInputStream(file);
+                inStream = new FileInputStream(file);
             } catch (Exception e) {
                 throw new UnexpectedException(e);
             }
             propsFromFile = IO.readUtf8Properties(inStream);
-         
+
             // Include
             Map<Object, Object> toInclude = new HashMap<Object, Object>(16);
             for (Object key : propsFromFile.keySet()) {
@@ -89,13 +91,13 @@ public class MessagesPlugin extends PlayPlugin {
                     try {
                         String filenameToInclude = propsFromFile.getProperty(key.toString());
                         File fileToInclude = getIncludeFile(file, filenameToInclude);
-                        if(fileToInclude != null && fileToInclude.exists() && !fileToInclude.isDirectory()){
+                        if (fileToInclude != null && fileToInclude.exists() && !fileToInclude.isDirectory()) {
                             // Check if the file was not previously read
-                            if(!includeMessageFilenames.contains(fileToInclude.getAbsolutePath())){
-                        	toInclude.putAll( read(fileToInclude) );
-                        	includeMessageFilenames.add(fileToInclude.getAbsolutePath());
+                            if (!includeMessageFilenames.contains(fileToInclude.getAbsolutePath())) {
+                                toInclude.putAll(read(fileToInclude));
+                                includeMessageFilenames.add(fileToInclude.getAbsolutePath());
                             }
-                        }else{
+                        } else {
                             Logger.warn("Missing include: %s from file %s", filenameToInclude, file.getPath());
                         }
                     } catch (Exception ex) {
@@ -109,13 +111,13 @@ public class MessagesPlugin extends PlayPlugin {
     }
 
     private static File getIncludeFile(File file, String filenameToInclude) {
-        if(file != null && filenameToInclude != null && !filenameToInclude.isEmpty()){
+        if (file != null && filenameToInclude != null && !filenameToInclude.isEmpty()) {
             // Test absolute path
             File fileToInclude = new File(filenameToInclude);
-            if(fileToInclude.isAbsolute()){
-            return fileToInclude;
-            }else{
-            return new File(file.getParent() + filenameToInclude); 
+            if (fileToInclude.isAbsolute()) {
+                return fileToInclude;
+            } else {
+                return new File(file.getParent(), filenameToInclude); 
             }
         }
         return null;
@@ -123,36 +125,42 @@ public class MessagesPlugin extends PlayPlugin {
 
     @Override
     public void detectChange() {
-	VirtualFile vf = Play.getVirtualFile("conf/messages");
-        if (vf != null && vf.exists() && !vf.isDirectory() && vf.lastModified() > lastLoading) {
+        VirtualFile vf = Play.getVirtualFile("conf/messages");
+        if (vf != null && vf.exists() && !vf.isDirectory()
+                && vf.lastModified() > lastLoading) {
             onApplicationStart();
             return;
         }
-        for(VirtualFile module : Play.modules.values()) {
+        for (VirtualFile module : Play.modules.values()) {
             vf = module.child("conf/messages");
-            if(vf != null && vf.exists() && !vf.isDirectory() && vf.lastModified() > lastLoading) {
+            if (vf != null && vf.exists() && !vf.isDirectory()
+                    && vf.lastModified() > lastLoading) {
                 onApplicationStart();
                 return;
             }
         }
         for (String locale : Play.langs) {
             vf = Play.getVirtualFile("conf/messages." + locale);
-            if (vf != null && vf.exists() && !vf.isDirectory() && vf.lastModified() > lastLoading) {
+            if (vf != null && vf.exists() && !vf.isDirectory()
+                    && vf.lastModified() > lastLoading) {
                 onApplicationStart();
                 return;
             }
-            for(VirtualFile module : Play.modules.values()) {
-                vf = module.child("conf/messages."+locale);
-                if( vf != null && vf.exists() && !vf.isDirectory() && vf.lastModified() > lastLoading) {
+            for (VirtualFile module : Play.modules.values()) {
+                vf = module.child("conf/messages." + locale);
+                if (vf != null && vf.exists() && !vf.isDirectory()
+                        && vf.lastModified() > lastLoading) {
                     onApplicationStart();
                     return;
                 }
             }
         }
-        
+
         for (String includeFilename : includeMessageFilenames) {
             File fileToInclude = new File(includeFilename);
-            if (fileToInclude != null && fileToInclude.exists() && !fileToInclude.isDirectory() && fileToInclude.lastModified() > lastLoading) {
+            if (fileToInclude != null && fileToInclude.exists()
+                    && !fileToInclude.isDirectory()
+                    && fileToInclude.lastModified() > lastLoading) {
                 onApplicationStart();
                 return;
             }
--- samples-and-tests/just-test-cases/test/MessagesTest.java
+++ samples-and-tests/just-test-cases/test/MessagesTest.java
@@ -1,14 +1,34 @@
 
 import play.*;
+
 import org.junit.Test;
+
 import play.test.UnitTest;
+import play.i18n.Lang;
 import play.i18n.Messages;
 
 public class MessagesTest extends UnitTest {
 
     @Test
-    public void testIncludeMessaes() {
+    public void testIncludeMessagesDefault() {
+        Lang.set("");
         assertEquals("Username:", Messages.get("login.username"));  
-        assertEquals("Hello MessagesTest you are log in.", Messages.get("login.success", "MessagesTest"));     
+        assertEquals("Hello MessagesTest you are log in.", Messages.get("login.success", "MessagesTest"));    
+        assertEquals("Another message", Messages.get("another.message"));  
+    }
+    
+    @Test
+    public void testIncludeMessagesUndefinedLang() {
+        assertEquals("Username:", Messages.getMessage("ru", "login.username"));  
+        assertEquals("Hello MessagesTest you are log in.", Messages.getMessage("ru","login.success", "MessagesTest"));    
+        assertEquals("Another message", Messages.getMessage("ru","another.message"));  
+    }
+    
+    @Test
+    public void testIncludeMessagesFrench() {
+        Lang.set("fr");
+        assertEquals("Nom d'utilisateur&nbsp;:", Messages.get("login.username"));  
+        assertEquals("Bonjour MessagesTest vous êtes connecté.", Messages.get("login.success", "MessagesTest"));    
+        assertEquals("Un autre message", Messages.get("another.message"));  
     }
 }
