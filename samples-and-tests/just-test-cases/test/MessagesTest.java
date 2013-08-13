d6b8974c20a15c3c2ad7c209219dd393ab27ab25
Matthias Kurz <m.kurz@irregular.at>
2013-08-13 17:33:51 +0200

    [#1695] Fallback when retrieving messages

--- samples-and-tests/just-test-cases/test/MessagesTest.java
+++ samples-and-tests/just-test-cases/test/MessagesTest.java
@@ -35,4 +35,47 @@ public class MessagesTest extends UnitTest {
 	    Lang.set("");
         }	  
     }
+
+    @Test
+    public void testCountrySpecificMessage() {
+        try{
+            Lang.set("fr_CA");
+            assertEquals("Votre nom:", Messages.get("login.username"));
+        }finally{
+            Lang.set("");
+        }
+    }
+
+    @Test
+    public void testLanguageFallbackFrench() {
+        try{
+            Lang.set("fr");
+            assertEquals("Your account has expired.", Messages.get("login.accountExpired")); // doesn't exist in french - fallback to default
+        }finally{
+            Lang.set("");
+        }
+    }
+
+    @Test
+    public void testLanguageFallbackFrenchCanadian() {
+        try{
+            Lang.set("fr_CA");
+            assertEquals("Bonjour MessagesTest vous êtes connecté.", Messages.get("login.success", "MessagesTest")); // doesn't exist in french canadian - fallback to french
+            assertEquals("Your account has expired.", Messages.get("login.accountExpired")); // doesn't exist in french and also not in french canadian - fallback to default
+        }finally{
+            Lang.set("");
+        }
+    }
+
+    @Test
+    public void testLanguageFallbackFrenchUndefinedCountry() {
+        try{
+            // using change() instead of set() to find closest match
+            Lang.change("fr_XY"); // fr_XY was not configured in application.conf, but fr was and therefore falling back to fr
+            assertEquals("fr", Lang.get());
+            assertEquals("Bonjour MessagesTest vous êtes connecté.", Messages.get("login.success", "MessagesTest"));
+        }finally{
+            Lang.set("");
+        }
+    }
 }
