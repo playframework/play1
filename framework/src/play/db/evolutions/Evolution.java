20252476f2a5f84ffab6dfe2fad2902a53a48697
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2013-08-13 12:00:43 +0800

    [#1662] Evolutions down from modules remove all others evolutions (modules, application) [#1641] Fix MySQL specific statement used in evolutions table update code [#1105] Fix again play_evolutions table creation for oracle db … [#1641] Fix MySQL specific statement to use in evolutions * add bin in .gitignore

---
+++ framework/src/play/db/evolutions/Evolution.java
@@ -0,0 +1,40 @@
+package play.db.evolutions;
+
+import play.libs.Codec;
+import java.sql.Date;
+
+
+public class Evolution implements Comparable<Evolution> {
+    public int revision;
+    public String sql_up;
+    public String sql_down;
+    public String hash;
+    public boolean applyUp;
+    
+    public String moduleKey;
+
+    public Evolution(String moduleKey, int revision, String sql_up, String sql_down,
+	    boolean applyUp) {
+	this.moduleKey = moduleKey;
+	this.revision = revision;
+	this.sql_down = sql_down;
+	this.sql_up = sql_up;
+	this.hash = Codec.hexSHA1(sql_up + sql_down);
+	this.applyUp = applyUp;
+    }
+
+    public int compareTo(Evolution o) {
+	return this.revision - o.revision;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	return (obj instanceof Evolution)
+		&& ((Evolution) obj).revision == this.revision;
+    }
+
+    @Override
+    public int hashCode() {
+	return revision;
+    }
+}
