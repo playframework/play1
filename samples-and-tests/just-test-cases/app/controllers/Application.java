ba5f3814f52d511c2b9171f67a879252f3168f34
Rich Hong <hong.rich@gmail.com>
2013-08-14 15:10:02 -0400

    [#1698] Fix select template tag to allow % and other non-String objects as label
    The % character needs to be escaped when used inside a messages tag
    (which the #{select} template tag does for label). In addition,
    labelProperty can point to a non-String field, so toString should be
    called on that before trying to HTML escape it.

--- samples-and-tests/just-test-cases/app/controllers/Application.java
+++ samples-and-tests/just-test-cases/app/controllers/Application.java
@@ -316,7 +316,7 @@ public class Application extends Controller {
     }
 
     public static void selectTag(){
-        List<User> users = new ArrayList<User>(10);
+        List<User> users = new ArrayList<User>(12);
         User user;
         for(long i = 0; i < 10; i++) {
         	user = new User("User-" + i);
@@ -324,6 +324,14 @@ public class Application extends Controller {
         	user.i = (int) i;
         	users.add(user);
         }
+        user = new User("User-%-10");
+        user.k = 10L;
+        user.i = (int) 10;
+        users.add(user);
+        user = new User("User-%%-11");
+        user.k = 11L;
+        user.i = (int) 11;
+        users.add(user);
         render(users);
     }
     
