1267f0370a733ae62c4bf0bd48ff4b360b985657
Jonathan Grimm <jon@toasttab.com>
2013-08-21 08:44:24 -0400

    [#1702] Using correct output data when serializing flash scope cookie

--- framework/src/play/mvc/Scope.java
+++ framework/src/play/mvc/Scope.java
@@ -66,7 +66,7 @@ public class Scope {
                 return;
             }
             try {
-                String flashData = CookieDataCodec.encode(data);
+                String flashData = CookieDataCodec.encode(out);
                 Http.Response.current().setCookie(COOKIE_PREFIX + "_FLASH", flashData, null, "/", null, COOKIE_SECURE);
             } catch (Exception e) {
                 throw new UnexpectedException("Flash serializationProblem", e);
