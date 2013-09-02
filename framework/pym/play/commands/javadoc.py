abe7ec5d255be1cd96e9024919b3294ba5ad5502
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2013-09-02 14:46:41 +0800

    [#1717] Fix javadoc fails if too much files to process

--- framework/pym/play/commands/javadoc.py
+++ framework/pym/play/commands/javadoc.py
@@ -16,30 +16,54 @@ def execute(**kargs):
     args = kargs.get("args")
     play_env = kargs.get("env")
 
-    app.check()
-    modules = app.modules()
     if not os.environ.has_key('JAVA_HOME'):
         javadoc_path = "javadoc"
     else:
         javadoc_path = os.path.normpath("%s/bin/javadoc" % os.environ['JAVA_HOME'])
-
+        
+    outdir = os.path.join(app.path, 'javadoc')
+    sout = open(os.path.join(app.log_path(), 'javadoc.log'), 'w')
+    serr = open(os.path.join(app.log_path(), 'javadoc.err'), 'w')
+    if (os.path.isdir(outdir)):
+        shutil.rmtree(outdir)
+        
+    defineJavadocOptions(app, outdir, args)
+    defineJavadocFiles(app)
+    javadoc_cmd = [javadoc_path, '@javadocOptions', '@javadocFiles']
+    
+    print "Generating Javadoc in " + outdir + "..."
+    subprocess.call(javadoc_cmd, env=os.environ, stdout=sout, stderr=serr)
+    print "Done! You can open " + os.path.join(outdir, 'overview-tree.html') + " in your browser."
+    
+    
+def defineJavadocOptions(app, outdir, args):
+    f = open('javadocOptions', 'w')
+    f.write(' '.join(['-classpath', app.cp_args(), '-d', outdir, '-encoding', 'UTF-8', '-charset', 'UTF-8']))
+    
+    argsCmd = ' '.join(args)
+    if (argsCmd != ''):
+        f.write(' ' + ' '.join(args))        
+    f.close()
+    
+def defineJavadocFiles(app):
     fileList = []
     def add_java_files(path):
         for root, subFolders, files in os.walk(path):	
             for file in files:
                  if file.endswith(".java"):
    	                fileList.append(os.path.join(root, file))
+                    
     add_java_files(os.path.join(app.path, "app"))
     add_java_files(os.path.join(app.path, "src"))
+    
+    # Javadoc of modules
+    modules = app.modules()  
     for module in modules:
 	    add_java_files(os.path.normpath(os.path.join(module, "app")))
 	    add_java_files(os.path.normpath(os.path.join(module, "src")))
-    outdir = os.path.join(app.path, 'javadoc')
-    sout = open(os.path.join(app.log_path(), 'javadoc.log'), 'w')
-    serr = open(os.path.join(app.log_path(), 'javadoc.err'), 'w')
-    if (os.path.isdir(outdir)):
-        shutil.rmtree(outdir)
-    javadoc_cmd = [javadoc_path, '-classpath', app.cp_args(), '-d', outdir, '-encoding', 'UTF-8', '-charset', 'UTF-8'] + args + fileList
-    print "Generating Javadoc in " + outdir + "..."
-    subprocess.call(javadoc_cmd, env=os.environ, stdout=sout, stderr=serr)
-    print "Done! You can open " + os.path.join(outdir, 'overview-tree.html') + " in your browser."
+        
+    #Write files list in files    
+    f = open('javadocFiles', 'w')
+    f.write(' '.join(fileList)) 
+    f.close()
+     
