72d69b4e73e8ce3954991e24c347590592b965e5
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2013-10-02 15:46:56 +0800

    [#988] Possibility to append java memory options to evolutions command * Add doc for evolutions command

--- framework/pym/play/commands/deps.py
+++ framework/pym/play/commands/deps.py
@@ -59,7 +59,6 @@ def execute(**kargs):
 
     java_cmd = [app.java_path()] + add_options + args_memory + ['-classpath', app.fw_cp_args(), 'play.deps.DependenciesManager']
     try:
-        print java_cmd
         return_code = subprocess.call(java_cmd, env=os.environ)
         if 0 != return_code:
             sys.exit(return_code);
