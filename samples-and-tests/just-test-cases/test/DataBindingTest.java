ac806cd14c20584d82e39c03a95d50e8608debb5
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2013-10-09 10:50:39 +0800

    [#899] Unbinder retrieve annotations from model Add test for unbind with TypeBinder (test fails => need to discuss) Comment the test as this issue is fixed [#1735] Comment failing test for unbind with TypeBinder (wait for the fix)

--- samples-and-tests/just-test-cases/test/DataBindingTest.java
+++ samples-and-tests/just-test-cases/test/DataBindingTest.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
+
 import play.libs.WS;
 import play.mvc.Http;
+import play.mvc.Http.Response;
 import play.mvc.Router;
 import play.test.FunctionalTest;
 import play.test.UnitTest;
@@ -23,8 +28,25 @@ public class DataBindingTest extends FunctionalTest {
         assertIsOk(response);
         assertContentEquals("MyBook[23],MyBook[12],MyBook[43],null,null,null,MyBook[35],null,MyBook[32],null,MyBook[1]", response);
     }
+    
+    @Test
+    public void testEditAnEntity() {
+	Map<String, String> params = new HashMap<String, String>();
+	params.put("entity.date", "2013-10-03 11:33:05:125 AM");
+	params.put("entity.yop", "yop");
+        Response response = POST(Router.reverse("DataBinding.editAnEntity").url, params);
+        assertIsOk(response);
+        assertContentMatch("2013-10-03 11:33:05:125 AM", response);
+        assertContentMatch("--yop--", response);
+    }
 
-
-
+    @Test
+    public void testDispatchAnEntity() {
+	Map<String, String> params = new HashMap<String, String>();
+	params.put("entity.date", "2013-10-03 11:33:05:125 AM");
+	params.put("entity.yop", "yop");
+        Response response = POST(Router.reverse("DataBinding.dispatchAnEntity").url, params);
+        assertStatus(302, response);
+    }
 }
 
