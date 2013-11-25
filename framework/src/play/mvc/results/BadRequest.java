d15d7d05e34b1ad98aaf6c7ebfac6bb68e9016e3
Dean Hiller <dean@alvazan.com>
2013-11-25 09:11:47 -0700

    add a way to tell user why the request was bad (clients of our webservice wanted messages of why the request was bad)

--- framework/src/play/mvc/results/BadRequest.java
+++ framework/src/play/mvc/results/BadRequest.java
@@ -9,6 +9,13 @@ import play.mvc.Http.Response;
  */
 public class BadRequest extends Result {
 
+	public BadRequest(String msg) {
+		super(msg);
+	}
+	
+	public BadRequest() {
+	}
+	
     @Override
     public void apply(Request request, Response response) {
         response.status = Http.StatusCode.BAD_REQUEST;
