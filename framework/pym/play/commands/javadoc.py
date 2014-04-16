3b7d7be90cfac1f60e722aacca577e8f5da25ef5
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2014-04-16 11:34:31 +0800

    [#1800] Update javadoc to link to Java8 [#1800] Add example for java8
    Conflicts:
    	framework/pym/play/commands/javadoc.py

--- framework/pym/play/commands/javadoc.py
+++ framework/pym/play/commands/javadoc.py
@@ -1,10 +1,13 @@
 import os, os.path
 import shutil
 import subprocess
+import re
 
 from play.utils import *
 
 COMMANDS = ['javadoc', 'jd']
+
+DEFAULT_API_VERSION = "1.3.0"
 
 HELP = {
     'javadoc': 'Generate your application Javadoc'
@@ -22,30 +25,77 @@ def execute(**kargs):
         javadoc_path = os.path.normpath("%s/bin/javadoc" % os.environ['JAVA_HOME'])
         
     outdir = os.path.join(app.path, 'javadoc')
-    sout = open(os.path.join(app.log_path(), 'javadoc.log'), 'w')
-    serr = open(os.path.join(app.log_path(), 'javadoc.err'), 'w')
+    # Clean current javadoc directory
     if (os.path.isdir(outdir)):
         shutil.rmtree(outdir)
-        
+    sout = open(os.path.join(app.log_path(), 'javadoc.log'), 'w')
+    serr = open(os.path.join(app.log_path(), 'javadoc.err'), 'w')
+
+    # Create Javadoc directory    
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)      
     defineJavadocOptions(app, outdir, args)
-    defineJavadocFiles(app)
-    javadoc_cmd = [javadoc_path, '@javadocOptions', '@javadocFiles']
+    defineJavadocFiles(app, outdir)
+    javadoc_cmd = [javadoc_path, '@'+os.path.join('javadoc','javadocOptions'), '@'+os.path.join('javadoc','javadocFiles')]
     
     print "Generating Javadoc in " + outdir + "..."
     subprocess.call(javadoc_cmd, env=os.environ, stdout=sout, stderr=serr)
     print "Done! You can open " + os.path.join(outdir, 'overview-tree.html') + " in your browser."
+    # Remove configuration file
+    os.remove(os.path.join(outdir , 'javadocOptions'))
+    os.remove(os.path.join(outdir , 'javadocFiles'))
+    
     
     
 def defineJavadocOptions(app, outdir, args):
-    f = open('javadocOptions', 'w')
+    f = open(os.path.join(outdir , 'javadocOptions'), 'w')
     f.write(' '.join(['-classpath', app.cp_args(), '-d', outdir, '-encoding', 'UTF-8', '-charset', 'UTF-8']))
-    
+   
+    # Add some default options
+    if args.count('-doctitle') == 0:
+        f.write(' -doctitle "' + app.readConf('application.name')  + '"' )
+        
+    if args.count('-header') == 0:    
+        f.write(' -header "<b>' +  app.readConf('application.name') + '</b>"')
+        
+    if args.count('-footer') == 0:    
+        f.write(' -footer "<b>' +  app.readConf('application.name') + '</b>"')
+  
+    if args.count('--links'):
+        print "~ Build project Javadoc with links to :"
+        args.remove('--links')
+        # Add link to JavaDoc of JAVA
+        
+        javaVersion = getJavaVersion()
+        if javaVersion == "1.5":
+            print "~    Java(TM) Platform, Platform Standard Edition 5.0"        
+            print "~    Java(TM) EE 5 Specification APIs"
+            f.write(' -link http://docs.oracle.com/javase/1.5.0/docs/api/')
+            f.write(' -link http://docs.oracle.com/javaee/5/api/')   
+        else:
+            urlVersion = javaVersion[2:]
+            print "~    Java(TM) Platform, Standard Edition " + urlVersion + " API Specification"        
+            print "~    Java(TM) EE " + urlVersion + "Specification APIs"
+            f.write(' -link http://docs.oracle.com/javase/' + urlVersion + '/docs/api/')
+            f.write(' -link http://docs.oracle.com/javaee/' + urlVersion + '/api/')         
+     
+       
+        # Add link to JavaDoc of Play Framework
+        playVersion = app.play_env['version']
+        if "localbuild" in playVersion:
+            print "~    API documentation to Play! Framework V" + playVersion + " doesn't exist => link to V" + DEFAULT_API_VERSION
+            playVersion = DEFAULT_API_VERSION
+
+        print "~    Play Framework V" + playVersion + " API documentation"     
+        f.write(' -link http://www.playframework.com/documentation/' + playVersion + '/api/')
+
+   
     argsCmd = ' '.join(args)
     if (argsCmd != ''):
         f.write(' ' + ' '.join(args))        
     f.close()
     
-def defineJavadocFiles(app):
+def defineJavadocFiles(app, outdir):
     fileList = []
     def add_java_files(path):
         for root, subFolders, files in os.walk(path):	
@@ -63,7 +113,7 @@ def defineJavadocFiles(app):
 	    add_java_files(os.path.normpath(os.path.join(module, "src")))
         
     #Write files list in files    
-    f = open('javadocFiles', 'w')
+    f = open(os.path.join(outdir, 'javadocFiles'), 'w')
     f.write(' '.join(fileList)) 
     f.close()
-     
+  
