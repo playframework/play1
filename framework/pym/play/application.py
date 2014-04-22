ea7a14ae432d24ac3f73c5de1c70d9037db8c5c0
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2014-04-22 08:45:13 +0800

    Revert "Support Java 8." Revert [#1800] Update compiler for support to java 8
    This reverts commit 3d604052ab2e83c0913244f3782a6f369e143d27.
    This reverts commit 3d3c3fe45733147f5478a70bd58891f163647f51
    This reverts commit e9d80946f4198bc7239f506e76e54c3c40470193
    This reverts commit 4521b8026b13ada18439120af2f8a4e8a79b7471
    This reverts commit 021d275545b76ebac43be623143ec85fe7f52119
    This reverts commit 3b7d7be90cfac1f60e722aacca577e8f5da25ef5

--- framework/pym/play/application.py
+++ framework/pym/play/application.py
@@ -1,6 +1,5 @@
 import sys
-import os
-import os.path
+import os, os.path
 import re
 import shutil
 import socket
@@ -262,14 +261,8 @@ class PlayApplication(object):
 
         if application_mode == 'prod':
             java_args.append('-server')
-
-        javaVersion = getJavaVersion()
-        if javaVersion == "1.7":
-            # JDK 7 compat
-            java_args.append('-XX:-UseSplitVerifier')
-        elif javaVersion == "1.8":
-            java_args.append('-noverify')
-
+        # JDK 7 compat
+        java_args.append('-XX:-UseSplitVerifier')
         java_policy = self.readConf('java.policy')
         if java_policy != '':
             policyFile = os.path.join(self.path, 'conf', java_policy)
