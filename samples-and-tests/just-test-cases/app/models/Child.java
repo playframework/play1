ef871db0a78484a9f194e0caeb1c16937bc9ad17
Alexandre Chatiron <alexandre.chatiron@gmail.com>
2014-04-24 17:21:06 +0800

    [#385] Fix NoBinding and Fixtures : create a special profile for fixture to handle nobinding when loading fixtures [#1804] Fix NoBinding not working properly on entity, JPAPlugin must check the noBinding annotation
    Conflicts:
    	framework/src/play/db/jpa/JPAPlugin.java

--- samples-and-tests/just-test-cases/app/models/Child.java
+++ samples-and-tests/just-test-cases/app/models/Child.java
@@ -1,13 +1,39 @@
 package models;
 
 import play.*;
+import play.data.binding.NoBinding;
 import play.db.jpa.*;
+
 import javax.persistence.*;
+
 import java.util.*;
 
 @Entity
 public class Child extends Model {
 
        public String name;
+       
+       @NoBinding("secure")
+       @ManyToOne
+       public Parent father;
+       
+       @NoBinding("secure")
+       @ManyToOne
+       public Parent mother;
+       
+       @NoBinding("secure")
+       @ManyToOne
+       public Person tutor;
+       
+       @NoBinding("Fixtures")
+       public String test;
+       
+       public String toSimpleJSON(){
+	   return "{\"name\":\"" + this.name  + "\"" + 
+		   ", \"father\":\"" + (this.father != null? this.father.name: "null") + "\"" + 
+		   ", \"mother\":\"" + (this.mother != null? this.mother.name: "null") + "\"" + 
+		   ", \"tutor\":\"" + (this.tutor != null? this.tutor.userName: "null") + "\"" + 
+		   "}";
+       }
 
 }
