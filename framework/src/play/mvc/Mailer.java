e690c4f62ba111d21dac921ceef997d57ab28409
tazmaniax <chris@spinthewebb.com>
2015-01-07 15:54:01 +1100

    [#1897] Added example/test

--- framework/src/play/mvc/Mailer.java
+++ framework/src/play/mvc/Mailer.java
@@ -146,7 +146,7 @@ public class Mailer implements LocalVariablesSupport {
 			throw new UnexpectedException("Mailer not instrumented ?");
 		}
 		
-		InlineImage inlinedImage = new InlineImage(dataSource);
+		InlineImage inlineImage = new InlineImage(dataSource);
 		
 		Map<String, InlineImage> inlineEmbeds = (Map<String, InlineImage>) map.get("inlineEmbeds");
 		if (inlineEmbeds == null) {
@@ -154,10 +154,10 @@ public class Mailer implements LocalVariablesSupport {
 			map.put("inlineEmbeds", inlineEmbeds);
 		}
 		
-		inlineEmbeds.put(name, inlinedImage);
+		inlineEmbeds.put(name, inlineImage);
 		infos.set(map);
 		
-		return "cid:" + inlinedImage.cid;
+		return "cid:" + inlineImage.cid;
 	}
 
     public static void setContentType(String contentType) {
