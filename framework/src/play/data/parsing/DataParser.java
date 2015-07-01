b006d7f6f49ebfb6765124ec44cf8e95b62f7c9f
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-07-01 10:10:36 +0300

    move method putMapEntry() from DataParser to class ApacheMultipartParser where it's actually used

--- framework/src/play/data/parsing/DataParser.java
+++ framework/src/play/data/parsing/DataParser.java
@@ -10,17 +10,4 @@ public abstract class DataParser {
 
     public abstract Map<String, String[]> parse(InputStream is);
 
-    public static void putMapEntry(Map<String, String[]> map, String name, String value) {
-        String[] newValues = null;
-        String[] oldValues = map.get(name);
-        if (oldValues == null) {
-            newValues = new String[1];
-            newValues[0] = value;
-        } else {
-            newValues = new String[oldValues.length + 1];
-            System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
-            newValues[oldValues.length] = value;
-        }
-        map.put(name, newValues);
-    }
 }
