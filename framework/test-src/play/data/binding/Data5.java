41bbf043ffa36b0917d73e949aa2b7852b197946
Sebastian Beigel <sebastian@beigel.de>
2015-12-28 14:14:15 +0100

    EnumSet binding test added

---
+++ framework/test-src/play/data/binding/Data5.java
@@ -0,0 +1,13 @@
+package play.data.binding;
+
+import java.util.EnumSet;
+
+public class Data5 {
+
+    public enum TestEnum {
+        A, B, C, D, E, F
+    }
+
+    public String s;
+    public EnumSet<TestEnum> testEnumSet;
+}
