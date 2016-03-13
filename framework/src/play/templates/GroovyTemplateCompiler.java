07162e8688b684fa77f53a8f0687b76a16769d40
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-03-13 23:36:32 +0200

    never ever swallow exceptions!

--- framework/src/play/templates/GroovyTemplateCompiler.java
+++ framework/src/play/templates/GroovyTemplateCompiler.java
@@ -356,12 +356,9 @@ public class GroovyTemplateCompiler extends TemplateCompiler {
             } catch (Exception e) {
                 // Use fastTag if exists
                 List<Class> fastClasses = new ArrayList<Class>();
-                try {
-                    fastClasses = Play.classloader.getAssignableClasses(FastTags.class);
-                } catch (Exception xe) {
-                    //
-                }
+                fastClasses = Play.classloader.getAssignableClasses(FastTags.class);
                 fastClasses.add(0, FastTags.class);
+                
                 Method m = null;
                 String tName = tag.name;
                 String tSpace = "";
