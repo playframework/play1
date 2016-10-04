62ab32348036dc2a50e62ebe3b9d1a7610ce90c4
Anton Keks <anton@azib.net>
2016-10-04 16:05:28 +0300

    Calculate template rendering time, so it can be logged/profiled separately from controller execution time.

--- framework/src/play/mvc/results/RenderTemplate.java
+++ framework/src/play/mvc/results/RenderTemplate.java
@@ -1,12 +1,12 @@
 package play.mvc.results;
 
-import java.util.Map;
-
 import play.exceptions.UnexpectedException;
 import play.libs.MimeTypes;
 import play.mvc.Http.Request;
 import play.mvc.Http.Response;
 import play.templates.Template;
+
+import java.util.Map;
 
 /**
  * 200 OK with a template rendering
@@ -15,13 +15,16 @@ public class RenderTemplate extends Result {
 
     private String name;
     private String content;
+    private long renderTime;
 
     public RenderTemplate(Template template, Map<String, Object> args) {
         this.name = template.name;
         if (args.containsKey("out")) {
             throw new RuntimeException("Assertion failed! args shouldn't contain out");
         }
+        long start = System.currentTimeMillis();
         this.content = template.render(args);
+        this.renderTime = System.currentTimeMillis() - start;
     }
 
     @Override
@@ -39,4 +42,7 @@ public class RenderTemplate extends Result {
         return content;
     }
 
+    public long getRenderTime() {
+        return renderTime;
+    }
 }
