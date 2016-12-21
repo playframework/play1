ed1bea8e19257bbead8d83f88a1041d4fe1676a7
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-12-22 01:55:53 +0200

    fix code formatting

--- framework/src/play/server/ServletWrapper.java
+++ framework/src/play/server/ServletWrapper.java
@@ -159,8 +159,8 @@ public class ServletWrapper extends HttpServlet implements ServletContextListene
             serveStatic(httpServletResponse, httpServletRequest, e);
             return;
         } catch(URISyntaxException e) {
-			 serve404(httpServletRequest, httpServletResponse, new NotFound(e.toString()));
-	         return;
+            serve404(httpServletRequest, httpServletResponse, new NotFound(e.toString()));
+            return;
         } catch (Throwable e) {
             throw new ServletException(e);
         } finally {
@@ -247,8 +247,8 @@ public class ServletWrapper extends HttpServlet implements ServletContextListene
     }
 
     public static Request parseRequest(HttpServletRequest httpServletRequest) throws Exception {
-	 	
-		URI uri = new URI(httpServletRequest.getRequestURI());
+
+        URI uri = new URI(httpServletRequest.getRequestURI());
         String method = httpServletRequest.getMethod().intern();
         String path = uri.getPath();
         String querystring = httpServletRequest.getQueryString() == null ? "" : httpServletRequest.getQueryString();
--- framework/src/play/server/StreamChunkAggregator.java
+++ framework/src/play/server/StreamChunkAggregator.java
@@ -72,13 +72,12 @@ public class StreamChunkAggregator extends SimpleChannelUpstreamHandler {
                     currentMessage.setContent(new FileChannelBuffer(localFile));
                     this.out = null;
                     this.currentMessage = null;
-		            this.file.delete();
+                    this.file.delete();
                     this.file = null;
                     Channels.fireMessageReceived(ctx, currentMessage, e.getRemoteAddress());
                 }
             }
         }
-
     }
 }
 
