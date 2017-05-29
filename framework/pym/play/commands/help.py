2735b5beac60682a7cb21a4a4e62bbbc5899ef9f
Robert Rettig <robert@rettig.bayern>
2017-05-29 14:14:45 +0200

    feature: help for module commands

--- framework/pym/play/commands/help.py
+++ framework/pym/play/commands/help.py
@@ -1,6 +1,6 @@
 # Display help
 
-import sys, os
+import sys, os, re
 
 COMMANDS = ['help']
 
@@ -15,15 +15,27 @@ def execute(**kargs):
     play_env = kargs.get("env")
     cmdloader = kargs.get("cmdloader")
 
-    if len(sys.argv) == 3:
-        cmd = sys.argv[2]
+    if len(args) == 1:
+        cmd = args[0]
         help_file = os.path.join(play_env["basedir"], 'documentation', 'commands', 'cmd-%s.txt' % cmd)
         if os.path.exists(help_file):
             print open(help_file, 'r').read()
         else:
-            print '~ Oops, command \'%s\' not found. Try just \'play help\' to list all commands.' % cmd
-            print '~'
-            sys.exit(-1)
+            exists = False
+            slugCmd = re.sub('[-\s]+', '-', re.sub('[^\w\s-]', '', cmd.encode('ascii', 'ignore')).strip().lower())
+            for module in app.modules():
+                help_file = os.path\
+                    .join(module, 'documentation', 'commands',
+                        'cmd-%s.txt'
+                          % slugCmd)
+                exists = os.path.exists(help_file)
+                if exists:
+                    print open(help_file, 'r').read()
+                    break
+            if not exists:
+                print '~ Oops, command \'%s\' not found. Try just \'play help\' to list all commands.' % cmd
+                print '~'
+                sys.exit(-1)
     else:
         main_help(cmdloader.commands, play_env)
 
@@ -52,4 +64,21 @@ def main_help(commands, play_env):
     print "~"
 
 def isCore(mod, play_env):
-    return mod.__file__.find(play_env["basedir"]) == 0
+    path = os.path.realpath(mod.__file__)
+    directory = os.path.realpath(play_env["basedir"])
+
+    isCore = True
+    try:
+        relpath = os.path.relpath(path, directory)
+        if relpath.startswith(os.pardir):
+            isCore = False
+        else:
+            if relpath.startswith('modules'):
+                isCore = False
+            else:
+                isCore = mod.__file__.find(play_env["basedir"]) == 0
+    except ValueError:
+        isCore = False
+
+    return isCore
+
