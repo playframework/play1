5bd90b0452918b8e561188b1511be541c375dc57
Anton Keks <anton@codeborne.com>
2017-08-23 18:01:32 +0300

    Extract SessionStore interface with its default implementation CookieSessionStore
    This will allow adding of other session stores, e.g. cache or database.

--- framework/src/play/mvc/Scope.java
+++ framework/src/play/mvc/Scope.java
@@ -1,13 +1,5 @@
 package play.mvc;
 
-import java.lang.annotation.Annotation;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import play.Logger;
 import play.Play;
 import play.data.binding.Binder;
@@ -20,8 +12,11 @@ import play.exceptions.UnexpectedException;
 import play.i18n.Messages;
 import play.libs.Codec;
 import play.libs.Crypto;
-import play.libs.Time;
 import play.utils.Utils;
+
+import java.lang.annotation.Annotation;
+import java.net.URLEncoder;
+import java.util.*;
 
 /**
  * All application Scopes
@@ -157,56 +152,14 @@ public class Scope {
      * Session scope
      */
     public static class Session {
+        public static SessionStore store = new CookieSessionStore();
 
         static final String AT_KEY = "___AT";
         static final String ID_KEY = "___ID";
         static final String TS_KEY = "___TS";
 
         public static Session restore() {
-            try {
-                Session session = new Session();
-                Http.Cookie cookie = Http.Request.current().cookies.get(COOKIE_PREFIX + "_SESSION");
-                int duration = Time.parseDuration(COOKIE_EXPIRE);
-                long expiration = (duration * 1000l);
-
-                if (cookie != null && Play.started && cookie.value != null && !cookie.value.trim().equals("")) {
-                    String value = cookie.value;
-                    int firstDashIndex = value.indexOf("-");
-                    if (firstDashIndex > -1) {
-                        String sign = value.substring(0, firstDashIndex);
-                        String data = value.substring(firstDashIndex + 1);
-                        if (CookieDataCodec.safeEquals(sign, Crypto.sign(data, Play.secretKey.getBytes()))) {
-                            CookieDataCodec.decode(session.data, data);
-                        }
-                    }
-                    if (COOKIE_EXPIRE != null) {
-                        // Verify that the session contains a timestamp, and
-                        // that it's not expired
-                        if (!session.contains(TS_KEY)) {
-                            session = new Session();
-                        } else {
-                            if ((Long.parseLong(session.get(TS_KEY))) < System.currentTimeMillis()) {
-                                // Session expired
-                                session = new Session();
-                            }
-                        }
-                        session.put(TS_KEY, System.currentTimeMillis() + expiration);
-                    } else {
-                        // Just restored. Nothing changed. No cookie-expire.
-                        session.changed = false;
-                    }
-                } else {
-                    // no previous cookie to restore; but we may have to set the
-                    // timestamp in the new cookie
-                    if (COOKIE_EXPIRE != null) {
-                        session.put(TS_KEY, (System.currentTimeMillis() + expiration));
-                    }
-                }
-
-                return session;
-            } catch (Exception e) {
-                throw new UnexpectedException("Corrupted HTTP session from " + Http.Request.current().remoteAddress, e);
-            }
+            return store.restore();
         }
 
         Map<String, String> data = new HashMap<>(); // ThreadLocal access
@@ -241,35 +194,7 @@ public class Scope {
         }
 
         void save() {
-            if (Http.Response.current() == null) {
-                // Some request like WebSocket don't have any response
-                return;
-            }
-            if (!changed && SESSION_SEND_ONLY_IF_CHANGED && COOKIE_EXPIRE == null) {
-                // Nothing changed and no cookie-expire, consequently send
-                // nothing back.
-                return;
-            }
-            if (isEmpty()) {
-                // The session is empty: delete the cookie
-                if (Http.Request.current().cookies.containsKey(COOKIE_PREFIX + "_SESSION") || !SESSION_SEND_ONLY_IF_CHANGED) {
-                    Http.Response.current().setCookie(COOKIE_PREFIX + "_SESSION", "", null, "/", 0, COOKIE_SECURE, SESSION_HTTPONLY);
-                }
-                return;
-            }
-            try {
-                String sessionData = CookieDataCodec.encode(data);
-                String sign = Crypto.sign(sessionData, Play.secretKey.getBytes());
-                if (COOKIE_EXPIRE == null) {
-                    Http.Response.current().setCookie(COOKIE_PREFIX + "_SESSION", sign + "-" + sessionData, null, "/", null, COOKIE_SECURE,
-                            SESSION_HTTPONLY);
-                } else {
-                    Http.Response.current().setCookie(COOKIE_PREFIX + "_SESSION", sign + "-" + sessionData, null, "/",
-                            Time.parseDuration(COOKIE_EXPIRE), COOKIE_SECURE, SESSION_HTTPONLY);
-                }
-            } catch (Exception e) {
-                throw new UnexpectedException("Session serializationProblem", e);
-            }
+            store.save(this);
         }
 
         public void put(String key, String value) {
