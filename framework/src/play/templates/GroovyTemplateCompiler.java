b28fa4a7c6b7c440d00658f66c45281c87eba1c7
Thomas Parle <thomas.parle@gmail.com>
2018-08-03 01:57:24 +0200

    fix previous loading class method using _ character now reserved in jdk9+

--- framework/src/play/templates/GroovyTemplateCompiler.java
+++ framework/src/play/templates/GroovyTemplateCompiler.java
@@ -101,14 +101,14 @@ public class GroovyTemplateCompiler extends TemplateCompiler {
             if (names.size() <= 1 || source.contains("new ")) {
                 for (String cName : names) { // dynamic class binding
                     source = source.replaceAll("new " + Pattern.quote(cName) + "(\\([^)]*\\))",
-                            "_('" + originalNames.get(cName).replace("$", "\\$") + "').newInstance$1");
+                            "__loadClass('" + originalNames.get(cName).replace("$", "\\$") + "').newInstance$1");
                 }
             }
 
             if (names.size() <= 1 || source.contains("instanceof")) {
                 for (String cName : names) { // dynamic class binding
                     source = source.replaceAll("([a-zA-Z0-9.-_$]+)\\s+instanceof\\s+" + Pattern.quote(cName),
-                            "_('" + originalNames.get(cName).replace("$", "\\$") + "').isAssignableFrom($1.class)");
+                            "__loadClass('" + originalNames.get(cName).replace("$", "\\$") + "').isAssignableFrom($1.class)");
 
                 }
             }
@@ -149,7 +149,7 @@ public class GroovyTemplateCompiler extends TemplateCompiler {
         println(" extends play.templates.GroovyTemplate.ExecutableTemplate {");
         println("public Object run() { use(play.templates.JavaExtensions) {");
         for (String n : extensionsClassnames) {
-            println("use(_('" + n + "')) {");
+            println("use(__loadClass('" + n + "')) {");
         }
     }
 
