53f7738f79453c02c7f45fffdbdfdec0ded305bb
Thomas Parle <thomas.parle@gmail.com>
2021-04-29 19:14:33 +0200

    upgrade to python 3 : main script working, now needs to test commands

--- framework/pym/play/commands/help.py
+++ framework/pym/play/commands/help.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 # Display help
 
 import sys, os, re
@@ -19,7 +20,7 @@ def execute(**kargs):
         cmd = args[0]
         help_file = os.path.join(play_env["basedir"], 'documentation', 'commands', 'cmd-%s.txt' % cmd)
         if os.path.exists(help_file):
-            print open(help_file, 'r').read()
+            print(open(help_file, 'r').read())
         else:
             exists = False
             slugCmd = re.sub('[-\s]+', '-', re.sub('[^\w\s-]', '', cmd.encode('ascii', 'ignore')).strip().lower())
@@ -30,38 +31,38 @@ def execute(**kargs):
                           % slugCmd)
                 exists = os.path.exists(help_file)
                 if exists:
-                    print open(help_file, 'r').read()
+                    print(open(help_file, 'r').read())
                     break
             if not exists:
-                print '~ Oops, command \'%s\' not found. Try just \'play help\' to list all commands.' % cmd
-                print '~'
+                print('~ Oops, command \'%s\' not found. Try just \'play help\' to list all commands.' % cmd)
+                print('~')
                 sys.exit(-1)
     else:
         main_help(cmdloader.commands, play_env)
 
 def main_help(commands, play_env):
     modules_commands = []
-    print "~ For all commands, if the application is not specified, the current directory is used"
-    print "~ Use 'play help cmd' to get more help on a specific command"
-    print "~"
-    print "~ Core commands:"
-    print "~ ~~~~~~~~~~~~~~"
+    print("~ For all commands, if the application is not specified, the current directory is used")
+    print("~ Use 'play help cmd' to get more help on a specific command")
+    print("~")
+    print("~ Core commands:")
+    print("~ ~~~~~~~~~~~~~~")
     for cmd in sorted(commands):
         if not isCore(commands[cmd], play_env):
             modules_commands.append(cmd)
             continue
         if 'HELP' in dir(commands[cmd]) and cmd in commands[cmd].HELP:
-            print "~ " + cmd + (' ' * (16 - len(cmd))) + commands[cmd].HELP[cmd]
+            print("~ " + cmd + (' ' * (16 - len(cmd))) + commands[cmd].HELP[cmd])
     if len(modules_commands) > 0:
-        print "~"
-        print "~ Modules commands:"
-        print "~ ~~~~~~~~~~~~~~~~~"
+        print("~")
+        print("~ Modules commands:")
+        print("~ ~~~~~~~~~~~~~~~~~")
         for cmd in modules_commands:
             if 'HELP' in dir(commands[cmd]) and cmd in commands[cmd].HELP:
-                print "~ " + cmd + (' ' * (20 - len(cmd))) + commands[cmd].HELP[cmd]
-    print "~"
-    print "~ Also refer to documentation at https://www.playframework.com/documentation"
-    print "~"
+                print("~ " + cmd + (' ' * (20 - len(cmd))) + commands[cmd].HELP[cmd])
+    print("~")
+    print("~ Also refer to documentation at https://www.playframework.com/documentation")
+    print("~")
 
 def isCore(mod, play_env):
     path = os.path.realpath(mod.__file__)
--- framework/pym/play/commands/war.py
+++ framework/pym/play/commands/war.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+from builtins import str
 import sys
 import os
 import getopt
@@ -32,50 +34,50 @@ def execute(**kargs):
                 war_zip_path = war_path + '.war'
             if o in ('--exclude'):
                 war_exclusion_list = a.split(':')
-                print "~ Excluding these directories :"
+                print("~ Excluding these directories :")
                 for excluded in war_exclusion_list:
-                    print "~  %s" %excluded
-    except getopt.GetoptError, err:
-        print "~ %s" % str(err)
-        print "~ Please specify a path where to generate the WAR, using the -o or --output option."
-        print "~ To exclude some directories, use the --exclude option and ':'-separator (eg: --exclude .svn:target:logs:tmp)."
-        print "~ "
+                    print("~  %s" %excluded)
+    except getopt.GetoptError as err:
+        print("~ %s" % str(err))
+        print("~ Please specify a path where to generate the WAR, using the -o or --output option.")
+        print("~ To exclude some directories, use the --exclude option and ':'-separator (eg: --exclude .svn:target:logs:tmp).")
+        print("~ ")
         sys.exit(-1)
 
     if not war_path:
-        print "~ Oops. Please specify a path where to generate the WAR, using the -o or --output option"
-        print "~ To exclude some directories, use the --exclude option and ':'-separator (eg: --exclude .svn:target:logs:tmp)."
-        print "~"
+        print("~ Oops. Please specify a path where to generate the WAR, using the -o or --output option")
+        print("~ To exclude some directories, use the --exclude option and ':'-separator (eg: --exclude .svn:target:logs:tmp).")
+        print("~")
         sys.exit(-1)
 
     if os.path.exists(war_path) and not os.path.exists(os.path.join(war_path, 'WEB-INF')):
-        print "~ Oops. The destination path already exists but does not seem to host a valid WAR structure"
-        print "~"
+        print("~ Oops. The destination path already exists but does not seem to host a valid WAR structure")
+        print("~")
         sys.exit(-1)
 
     if isParentOf(app.path, war_path) and not isExcluded(war_path, war_exclusion_list):
-        print "~ Oops. Please specify a destination directory outside of the application"
-        print "~ or exclude war destination directory using the --exclude option and ':'-separator "
-        print "~ (eg: --exclude .svn:target:logs:tmp)."
-        print "~"
+        print("~ Oops. Please specify a destination directory outside of the application")
+        print("~ or exclude war destination directory using the --exclude option and ':'-separator ")
+        print("~ (eg: --exclude .svn:target:logs:tmp).")
+        print("~")
         sys.exit(-1)
 
     # Precompile first
     precompilation_result = play.commands.precompile.execute(command=command, app=app, args=args, env=env)
 
     if precompilation_result != 0:
-        print "~ Please fix compilation errors before packaging WAR"
-        print "~"
+        print("~ Please fix compilation errors before packaging WAR")
+        print("~")
         sys.exit(precompilation_result)
 
     # Package
     package_as_war(app, env, war_path, war_zip_path, war_exclusion_list)
 
-    print "~ Done !"
-    print "~"
-    print "~ You can now load %s as a standard WAR into your servlet container" % (os.path.normpath(war_path))
-    print "~ You can't use play standard commands to run/stop/debug the WAR application..."
-    print "~ ... just use your servlet container commands instead"
-    print "~"
-    print "~ Have fun!"
-    print "~"
+    print("~ Done !")
+    print("~")
+    print("~ You can now load %s as a standard WAR into your servlet container" % (os.path.normpath(war_path)))
+    print("~ You can't use play standard commands to run/stop/debug the WAR application...")
+    print("~ ... just use your servlet container commands instead")
+    print("~")
+    print("~ Have fun!")
+    print("~")
