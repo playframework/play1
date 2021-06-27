a481dd38c6796784c1c621b08932458ca0bcbaf8
Chris Webb <chris@spinthewebb.com>
2021-06-27 13:42:13 +0100

    Updated dependencies and allow support for JDK 16 source

--- samples-and-tests/just-test-cases/test/classloading/enhancers/PropertiesEnhancerTest.java
+++ samples-and-tests/just-test-cases/test/classloading/enhancers/PropertiesEnhancerTest.java
@@ -22,62 +22,62 @@ import org.junit.Test;
 import play.test.FunctionalTest;
 import play.test.UnitTest;
 
-
 public class PropertiesEnhancerTest extends FunctionalTest {
 
-    @Test
-    public void testEnhancerMethods() throws Exception {
-	Project obj = new Project();
-	// get all of the properties for a POJO
-	PropertyDescriptor[] descriptors = PropertyUtils
-		.getPropertyDescriptors(obj);
-      
-	assertEquals(10, descriptors.length);
+	@Test
+	public void testEnhancerMethods() throws Exception {
+		Project obj = new Project();
+		// get all of the properties for a POJO
+		PropertyDescriptor[] descriptors = PropertyUtils.getPropertyDescriptors(obj);
 
-	assertTrue(validAccessorFor(obj, descriptors, "class", true, false));
-	assertTrue(validAccessorFor(obj, descriptors, "companies", true, true));
-	assertTrue(validAccessorFor(obj, descriptors, "company", true, true));
-	assertTrue(validAccessorFor(obj, descriptors, "endDate", true, true));
-	assertTrue(validAccessorFor(obj, descriptors, "entityId", true, false));
-	assertTrue(validAccessorFor(obj, descriptors, "id", true, false));
-	assertTrue(validAccessorFor(obj, descriptors, "name", true, true));
-	assertTrue(validAccessorFor(obj, descriptors, "observation", true, true));
-	assertTrue(validAccessorFor(obj, descriptors, "startDate", true, true));
-    }
+		assertEquals(9, descriptors.length);
 
-    private boolean validAccessorFor(Object obj,
-	    PropertyDescriptor[] descriptors, String name, boolean readMethod,
-	    boolean writeMethod) {
-	// go through all values
-	for (int i = 0; i < descriptors.length; i++) {
-	    PropertyDescriptor descriptor = descriptors[i];
-	    if (descriptor.getName().equals(name)) {
-		if (readMethod) {
-		    Method method = descriptor.getReadMethod();
-		    if (method == null) {
-			return false;
-		    }
-		}
-
-		if (writeMethod) {
-		    Method method = descriptor.getWriteMethod();
-		    if (method == null) {
-			return false;
-		    }
-		}
-		return true;
-	    }
+// It appears the "class" attribute is no longer returned in BeanUtils v1.9.4. This property is only
+// referenced in play.db.jpa.JPAModelLoader.makeCompositeKey(Model model) and it is specifically ignored
+// so not required.
+//		assertTrue(validAccessorFor(obj, descriptors, "class", true, false));
+		assertTrue(validAccessorFor(obj, descriptors, "companies", true, true));
+		assertTrue(validAccessorFor(obj, descriptors, "company", true, true));
+		assertTrue(validAccessorFor(obj, descriptors, "endDate", true, true));
+		assertTrue(validAccessorFor(obj, descriptors, "entityId", true, false));
+		assertTrue(validAccessorFor(obj, descriptors, "id", true, false));
+		assertTrue(validAccessorFor(obj, descriptors, "name", true, true));
+		assertTrue(validAccessorFor(obj, descriptors, "observation", true, true));
+		assertTrue(validAccessorFor(obj, descriptors, "startDate", true, true));
 	}
-	return false;
-    }
 
-    @Test
-    public void testEnhancerProperty() throws Exception {
-	Project obj = new Project();
-	obj.name = "toto";
-	Object value = PropertyUtils.getProperty(obj, "name");
+	private boolean validAccessorFor(Object obj, PropertyDescriptor[] descriptors, String name, boolean readMethod,
+			boolean writeMethod) {
+		// go through all values
+		for (int i = 0; i < descriptors.length; i++) {
+			PropertyDescriptor descriptor = descriptors[i];
+			if (descriptor.getName().equals(name)) {
+				if (readMethod) {
+					Method method = descriptor.getReadMethod();
+					if (method == null) {
+						return false;
+					}
+				}
 
-	assertNotNull(value);
-	assertEquals("toto", value);
-    }
+				if (writeMethod) {
+					Method method = descriptor.getWriteMethod();
+					if (method == null) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Test
+	public void testEnhancerProperty() throws Exception {
+		Project obj = new Project();
+		obj.name = "toto";
+		Object value = PropertyUtils.getProperty(obj, "name");
+
+		assertNotNull(value);
+		assertEquals("toto", value);
+	}
 }
