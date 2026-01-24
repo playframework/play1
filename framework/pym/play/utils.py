80c320e39c76a04e1a67097a2c73b83bdf16a4e0
andrey alekseenko <skelandy@gmail.com>
2026-01-25 03:55:29 +0600

    use raw strings to prevent "invalid escape sequence '\<char>'"

--- framework/pym/play/utils.py
+++ framework/pym/play/utils.py
@@ -17,11 +17,11 @@ def playVersion(play_env):
 
 def replaceAll(file, searchExp, replaceExp, regexp=False):
     if not regexp:
-        replaceExp = replaceExp.replace('\\', '\\\\')
-        searchExp = searchExp.replace('$', '\\$')
-        searchExp = searchExp.replace('{', '\\{')
-        searchExp = searchExp.replace('}', '\\}')
-        searchExp = searchExp.replace('.', '\\.')
+        replaceExp = replaceExp.replace('\\', r'\\')
+        searchExp = searchExp.replace('$', r'\$')
+        searchExp = searchExp.replace('{', r'\{')
+        searchExp = searchExp.replace('}', r'\}')
+        searchExp = searchExp.replace('.', r'\.')
     for line in fileinput.input(file, inplace=1):
         line = re.sub(searchExp, replaceExp, line)
         sys.stdout.write(line)
@@ -44,7 +44,7 @@ def isParentOf(path1, path2):
         sep = os.sep
         if sep == '\\':
             sep = '\\\\'
-        ptn = '^\.\.(' + sep + '\.\.)*$'
+        ptn = r'^\.\.(' + sep + r'\.\.)*$'
         return re.match(ptn, relpath) != None
     except:
         return False
@@ -255,7 +255,7 @@ def get_java_version():
     java_version = sp.communicate()
     java_version = str(java_version)
     
-    result = re.search('version "([a-zA-Z0-9\.\-_]{1,})"', java_version)
+    result = re.search(r'version "([a-zA-Z0-9\.\-_]{1,})"', java_version)
     
     if result:
         return result.group(1)
